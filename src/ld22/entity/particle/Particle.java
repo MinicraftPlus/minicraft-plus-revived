388c88ff0d2ca653b03a1988af986c6e29b5363f
Christopher Johns <cjohns4288@gmail.com>
2017-03-17 15:56:46 -0700

    removed symbolic link error

--- src/ld22/entity/particle/Particle.java
+++
@@ -1,9 +0,0 @@
-package com.mojang.ld22.entity.particle;
-
-import com.mojang.ld22.entity.Entity;
-
-public class Particle extends Entity {
-	public Particle() {}
-
-	public void tick() {}
-}
--- src/ld22/screen/ListItem.java
+++
@@ -1,7 +0,0 @@
-package com.mojang.ld22.screen;
-
-import com.mojang.ld22.gfx.Screen;
-
-public interface ListItem {
-	void renderInventory(Screen screen, int i, int j);
-}
