c15adeb84445abdc8ce80152543b7280f1c33862
Christopher Johns <cjohns4288@gmail.com>
2017-03-17 21:44:52 -0700

    Commented Particle classes; added back FirePareParticle

--- src/com/mojang/ld22/entity/particle/Particle.java
+++ src/com/mojang/ld22/entity/particle/Particle.java
@@ -2,6 +2,7 @@ package com.mojang.ld22.entity.particle;
 
 import com.mojang.ld22.entity.Entity;
 
+//what is this for, anyway? None of the particle classes extend it...
 public class Particle extends Entity {
 	public Particle() {}
 
--- src/com/mojang/ld22/entity/particle/TextParticle.java
+++ src/com/mojang/ld22/entity/particle/TextParticle.java
@@ -6,20 +6,22 @@ import com.mojang.ld22.gfx.Font;
 import com.mojang.ld22.gfx.Screen;
 
 public class TextParticle extends Entity {
-	private String msg;
-	private int col;
-	private int time = 0;
-	public double xa, ya, za;
-	public double xx, yy, zz;
+	private String msg; // Message of the text particle
+	private int col; // Color of the text particle
+	private int time = 0; // time (in ticks) that the particle has been alive
+	public double xa, ya, za; // x,y,z acceleration
+	public double xx, yy, zz; // x,y,z coordinates
 
 	public TextParticle(String msg, int x, int y, int col) {
 		this.msg = msg;
 		this.x = x;
 		this.y = y;
 		this.col = col;
-		xx = x;
-		yy = y;
-		zz = 2;
+		xx = x; //assigns x pos
+		yy = y; //assigns y pos
+		zz = 2; //assigns z pos to be 2
+		
+		//assigns x,y,z acceleration:
 		xa = random.nextGaussian() * 0.3;
 		ya = random.nextGaussian() * 0.2;
 		za = random.nextFloat() * 0.7 + 2;
@@ -28,25 +30,29 @@ public class TextParticle extends Entity {
 	public void tick() {
 		time++;
 		if (time > 60) {
+			//remove text particle after 60 ticks.
 			remove();
 		}
+		//move the particle according to the acceleration
 		xx += xa;
 		yy += ya;
 		zz += za;
 		if (zz < 0) {
+			//if z pos if less than 0, alter accelerations...
 			zz = 0;
 			za *= -0.5;
 			xa *= 0.6;
 			ya *= 0.6;
 		}
-		za -= 0.15;
+		za -= 0.15;  // za decreases by 0.15 every tick.
+		//truncate x and y coordinates to integers:
 		x = (int) xx;
 		y = (int) yy;
 	}
 
 	public void render(Screen screen) {
 		//		Font.draw(msg, screen, x - msg.length() * 4, y, Color.get(-1, 0, 0, 0));
-		Font.draw(msg, screen, x - msg.length() * 4 + 1, y - (int) (zz) + 1, Color.get(-1, 0, 0, 0));
-		Font.draw(msg, screen, x - msg.length() * 4, y - (int) (zz), col);
+		Font.draw(msg, screen, x - msg.length() * 4 + 1, y - (int) (zz) + 1, Color.get(-1, 0, 0, 0)); //renders the backdrop
+		Font.draw(msg, screen, x - msg.length() * 4, y - (int) (zz), col); // renders the text
 	}
 }
