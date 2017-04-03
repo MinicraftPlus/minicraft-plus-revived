6132a6a2023d5b16f99bdc384d9bf1099fe4c5ca
Christopher Johns <cjohns4288@gmail.com>
2017-04-02 17:55:07 -0700

    commented Entity.java

--- src/com/mojang/ld22/entity/Entity.java
+++ src/com/mojang/ld22/entity/Entity.java
@@ -9,17 +9,14 @@ import java.util.List;
 import java.util.Random;
 
 public class Entity {
-	/// I'm pretty darn sure that entity coordinates are per pixel, not per tile; this has likely been the cause of the inaccuracy of the print locations.
+	/// entity coordinates are per pixel, not per tile; each tile is 16x16 entity pixels.
 	protected final Random random = new Random();
-	public int x;
-	public int y;
-	public static int xx; // static..?
-	public static int yy; // both never used, or so it seems.
-	public int xr = 6;
-	public int yr = 6;
-	public boolean removed;
-	public Level level;
-	public boolean hasspawned = false;
+	public int x, y; // x, y entity coordinates on the map
+	public static int xx, yy; // static...? both never used, or so it seems.
+	public int xr = 6, yr = 6; // x, y radius of entity
+	public boolean removed; // Determines if the entity should be removed from the level
+	public Level level; // the level that the entity is on
+	//public boolean hasspawned = false; //? if the entity has spawned in the level yet?
 	public int col = Color.get(-1, 0, 111, 222); // current Color
 	// day/night color variations, I think.
 	public int col0 = Color.get(-1, 0, 111, 222);
@@ -27,121 +24,154 @@ public class Entity {
 	public int col2 = Color.get(-1, 0, 111, 222);
 	public int col3 = Color.get(-1, 0, 111, 222);
 	public int col4 = Color.get(-1, 0, 111, 222);
-
+	
+	/** Render method, (Look in each specific entity's class) */
 	public void render(Screen screen) {}
 
 	public void tick() {}
-
+	
+	/** Removes the entity from the world... somehow. */
 	public void remove() {
 		removed = true;
 	}
-
+	
+	/** Initialization method, called when the entity is created */
 	public final void init(Level level) {
 		this.level = level;
 	}
-
+	
+	/** returns true if this entity is found in the rectangle specified by given two coordinates. */
 	public boolean intersects(int x0, int y0, int x1, int y1) {
+		// x0,y0 = upper-left corner of rect; x1,y1 = bottom-right corner of rect
 		return !(x + xr < x0 || y + yr < y0 || x - xr > x1 || y - yr > y1);
 	}
-
+	
+	/** Extended in Mob.java & Furniture.java */
 	public boolean blocks(Entity e) {
 		return false;
 	}
-
+	
+	/** Extended in Mob.java */
 	public void hurt(Mob mob, int dmg, int attackDir) {}
-
 	public void hurt(Tnt tnt, int x, int y, int dmg) {}
-
 	public void hurt(Tile tile, int x, int y, int dmg) {}
-
+		
+	/** Moves an entity horizontally and vertically. */
 	public boolean move(int xa, int ya) {
-		if (!level.player.game.saving && (xa != 0 || ya != 0)) {
-			boolean stopped = true;
-			if (xa != 0 && move2(xa, 0)) stopped = false;
-			if (ya != 0 && move2(0, ya)) stopped = false;
+		if (!level.player.game.saving && (xa != 0 || ya != 0)) { // if not saving, and the entity is actually going to move...
+			boolean stopped = true; // used to check if the entity has BEEN stopped, COMPLETELY; this checks for a lack of collision.
+			if (xa != 0 && move2(xa, 0)) stopped = false; // horizontal movement was successful.
+			if (ya != 0 && move2(0, ya)) stopped = false; // vertical movement was successful.
 			if (!stopped) {
-				int xt = x >> 4;
-				int yt = y >> 4;
-				level.getTile(xt, yt).steppedOn(level, xt, yt, this);
+				
+				/* I guess I should explain something real quick. The coordinates between tiles and entities are different.
+				 * The world coordinates for tiles is 128x128
+				 * The world coordinates for entities is 2048x2048
+				 * This is because each tile is 16x16 pixels big
+				 * 128 x 16 = 2048.
+				 * When ever you see a ">>", it means that it is a right shift operator. This means it shifts bits to the right (making them smaller)
+				 * x >> 4 is the equivalent to x / (2^4). Which means it's dividing the X value by 16. (2x2x2x2 = 16)
+				 * xt << 4 is the equivalent to xt * (2^4). Which means it's multiplying the X tile value by 16.
+				 *
+				 * These bit shift operators are used to easily get the X & Y coordinates of a tile that the entity is standing on. */
+				
+				int xt = x >> 4; // the x tile coordinate that the entity is standing on.
+				int yt = y >> 4; // the y tile coordinate that the entity is standing on.
+				level.getTile(xt, yt).steppedOn(level, xt, yt, this); // Calls the steppedOn() method in a tile's class. (used for tiles like sand (footprints) or lava (burning))
 			}
 			return !stopped;
 		}
-		return true;
+		return true; // reaches this if no movement was requested / game was saving.
 	}
-
+	
+	/** Second part to the move method (moves in one direction at a time) */
 	protected boolean move2(int xa, int ya) {
 		if (xa != 0 && ya != 0)
 			throw new IllegalArgumentException("Move2 can only move along one axis at a time!");
-
-		int xto0 = ((x) - xr) >> 4;
-		int yto0 = ((y) - yr) >> 4;
-		int xto1 = ((x) + xr) >> 4;
-		int yto1 = ((y) + yr) >> 4;
-
+		
+		// gets the tile coordinate of each direction from the sprite...
+		int xto0 = ((x) - xr) >> 4; // to the left
+		int yto0 = ((y) - yr) >> 4; // above
+		int xto1 = ((x) + xr) >> 4; // to the right
+		int yto1 = ((y) + yr) >> 4; // below
+		
+		// gets same as above, but after movement.
 		int xt0 = ((x + xa) - xr) >> 4;
 		int yt0 = ((y + ya) - yr) >> 4;
 		int xt1 = ((x + xa) + xr) >> 4;
 		int yt1 = ((y + ya) + yr) >> 4;
-		boolean blocked = false;
-		for (int yt = yt0; yt <= yt1; yt++)
-			for (int xt = xt0; xt <= xt1; xt++) {
-				if (xt >= xto0 && xt <= xto1 && yt >= yto0 && yt <= yto1) continue;
-				level.getTile(xt, yt).bumpedInto(level, xt, yt, this);
-				if (!level.getTile(xt, yt).mayPass(level, xt, yt, this)) {
-					blocked = true;
+		
+		//boolean blocked = false; // if the next tile can block you.
+		for (int yt = yt0; yt <= yt1; yt++) { // cycles through y's of tile after movement
+			for (int xt = xt0; xt <= xt1; xt++) { // cycles through x's of tile after movement
+				if (xt >= xto0 && xt <= xto1 && yt >= yto0 && yt <= yto1) continue; // skip this position if this entity's sprite is touching it
+				// tile positions that make it here are the ones that the entity will be in, but are not in now.
+				level.getTile(xt, yt).bumpedInto(level, xt, yt, this); // Used in tiles like cactus
+				if (!level.getTile(xt, yt).mayPass(level, xt, yt, this)) { // if the entity can't pass this tile...
+					//blocked = true; // then the entity is blocked
 					return false;
 				}
 			}
-		if (blocked) return false;
-
-		List<Entity> wasInside = level.getEntities(x - xr, y - yr, x + xr, y + yr);
-		List<Entity> isInside = level.getEntities(x + xa - xr, y + ya - yr, x + xa + xr, y + ya + yr);
-		for (int i = 0; i < isInside.size(); i++) {
-			Entity e = isInside.get(i);
-			if (e == this) continue;
-
-			e.touchedBy(this);
 		}
-		isInside.removeAll(wasInside);
+		//if (blocked) return false;
+		
+		// these lists are named as if the entity has already moved-- it hasn't, though.
+		List<Entity> wasInside = level.getEntities(x - xr, y - yr, x + xr, y + yr); // gets all of the entities that are inside this entity (aka: colliding) before moving.
+		List<Entity> isInside = level.getEntities(x + xa - xr, y + ya - yr, x + xa + xr, y + ya + yr); // gets the entities that this entity will touch once moved.
+		for (int i = 0; i < isInside.size(); i++) {
+			/// cycles through entites about to be touched, and calls touchedBy(this) for each of them.
+			Entity e = isInside.get(i);
+			if (e == this) continue; // touching yourself doesn't count.
+			
+			e.touchedBy(this); // call the method. ("touch" the entity)
+		}
+		isInside.removeAll(wasInside); // remove all the entites that this one is already touching before move.
 		for (int i = 0; i < isInside.size(); i++) {
 			Entity e = isInside.get(i);
 			if (e == this) continue;
 
-			if (e.blocks(this)) {
-				return false;
-			}
+			if (e.blocks(this)) return false; // if the entity can block this entity, then this can't move.
 		}
 		
-		// finally! THIS is where the move happens!
+		// finally, the entity moves!
 		x += xa;
 		y += ya;
-		return true;
+		return true; // the move was successful.
 	}
-
+	
+	/** if this entity is touched by another entity (extended by sub-classes) */
 	protected void touchedBy(Entity entity) {}
-
+	
+	/** returns if mobs can block this entity (aka: can't pass through them) */
 	public boolean isBlockableBy(Mob mob) {
-		return true;
+		return true; // yes, mobs block other entities.
 	}
-
+	
+	/** Used in ItenEntity.java, extended with Player.java */
 	public void touchItem(ItemEntity itemEntity) {}
-
+	
+	/** Determines if the entity can swim (extended in sub-classes) */
 	public boolean canSwim() {
 		return false;
 	}
-
+	
+	/** This, strangely enough, determines if the entity can walk on wool; among some other things..? */
 	public boolean canWool() {
+		// TODO I personally think this should be removed, and replaced in the wool mayPass mothod. Speaking of the Wool classes, I see no reason why they are more than one class...
 		return false;
 	}
-
+	
+	/** If the entity can light up..? */
 	public boolean canLight() {
 		return false;
 	}
-
+	
+	/** Item interact, used in player.java */
 	public boolean interact(Player player, Item item, int attackDir) {
 		return item.interact(player, this, attackDir);
 	}
-
+	
+	/** sees if the player has used an item in a direction (extended in player.java) */
 	public boolean use(Player player, int attackDir) {
 		return false;
 	}
