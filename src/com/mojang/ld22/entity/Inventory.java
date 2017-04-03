66c32e05f455fa7d382a5661ecd11785ec01cb99
Christopher Johns <cjohns4288@gmail.com>
2017-04-02 19:51:22 -0700

    commented Inventory.java

--- src/com/mojang/ld22/entity/Inventory.java
+++ src/com/mojang/ld22/entity/Inventory.java
@@ -10,45 +10,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inventory {
-	public List<Item> items = new ArrayList<Item>();
-	public static List<Item> itemss = new ArrayList<Item>();
-	public boolean playerinventory = false;
-
+	public List<Item> items = new ArrayList<Item>(); // the list of items that is in the inventory.
+	public static List<Item> itemss = new ArrayList<Item>(); // a static list, of the last inventory that was added to with add(Item).
+	public boolean playerinventory = false; // if this is a player inventory.
+	
 	public Inventory() {}
-
+		
 	public Inventory(Player player) {
 		this.playerinventory = true;
 	}
-
+	
+	/** Adds an item to the inventory */
 	public void add(Item item) {
-		add(items.size(), item);
-		itemss = items;
+		add(items.size(), item);  // adds the item to the end of the inventory list
+		itemss = items; // sets static inv. to this one.
 	}
-
+	
+	/** Adds an item to a specific spot in the inventory */
 	public void add(int slot, Item item) {
-		if (item instanceof ResourceItem) {
-			ResourceItem toTake = (ResourceItem) item;
-			ResourceItem has = findResource(toTake.resource);
-			if (has == null) {
-				items.add(slot, toTake);
+		if (item instanceof ResourceItem) { // if the item is a resource...
+			ResourceItem toTake = (ResourceItem) item; // ...convert it into a ResourceItem object.
+			ResourceItem has = findResource(toTake.resource); // finds if the resourceItem is already in their inventory (looking for an instance of ResourceItem with the same .resource)
+			if (has == null) { // if the owner of this inventory doesn't have the resource
+				items.add(slot, toTake); // add the resource in the items list
 			} else {
-				has.count += toTake.count;
+				has.count += toTake.count; // else add to the count of the item resource already there.
 			}
 		} else {
-			items.add(slot, item);
+			items.add(slot, item); // add the item to the items list
 		}
 	}
 	
+	/** Finds a resource in your inventory */
 	private ResourceItem findResource(Resource resource) {
+		/// this works becuase all resources are simply references to the static ones in Resource.java; so, multiple ResourceItems SHOULD just have different references to the same Resource object.
 		for (int i = 0; i < items.size(); i++) {
 			if (items.get(i) instanceof ResourceItem) {
 				ResourceItem has = (ResourceItem) items.get(i);
-				if (has.resource == resource) return has;
+				if (has.resource == resource) return has; // returns if the loop has found a ResourceItem with a matching resource in your inventory
 			}
 		}
-		return null;
+		return null; // else it will return null
 	}
 	
+	/** this is findResourceStatic, not multiple. It's like the one above. */
 	private static ResourceItem findResources(Resource resource) {
 		for (int i = 0; i < itemss.size(); i++) {
 			if (itemss.get(i) instanceof ResourceItem) {
@@ -59,10 +64,11 @@ public class Inventory {
 		return null;
 	}
 	
+	/** like findResource, but for other items. */
 	private Item findItem(Item item) {
 		for(int i = 0; i < this.items.size(); i++) {
 			Item has = (Item)this.items.get(i);
-			if(has.getName().equals(item.getName())) {
+			if(has.getName().equals(item.getName())) { // compares item name for match.
 				return has;
 			}
 		}
@@ -70,61 +76,68 @@ public class Inventory {
 		return null;
 	}
 	
-	private ToolItem findtool(ToolType type) {
+	/** Same as above for tools. */
+	private ToolItem findtool(ToolType type, int level, boolean matchHigher) { // may match if higher quality.
 		for (int i = 0; i < items.size(); i++) {
 			if (items.get(i) instanceof ToolItem) {
 				ToolItem hass = (ToolItem) items.get(i);
-				if (hass.type == type) return hass;
+				if (hass.type == type && (hass.level == level || hass.level > level && matchHigher) return hass; // compares ToolType and level for match.
 			}
 		}
 		return null;
 	}
-
+	
+	/** Returns true if the player has the resource, and has equal or more than the amount given */
 	public boolean hasResources(Resource r, int count) {
-		ResourceItem ri = findResource(r);
-		if (ri == null) return false;
-		return ri.count >= count;
-	}
-
-	public boolean hasTools(ToolType t, int level) {
-		ToolItem ti = findtool(t);
-		if (ti == null) return false;
-		return ti.level >= level;
+		ResourceItem ri = findResource(r); // finds the matching ResourceItem in your inv, if it exists.
+		if (ri == null) return false; // if the player doesn't have the ResourceItem, then return false.
+		return ri.count >= count; // otherwise, return whether the inventory has the given amount of the resource.
 	}
 	
+	/** true if this inv contains the given tool. */
+	public boolean hasTools(ToolType t, int level) {
+		ToolItem ti = findtool(t, level, false);
+		if (ti == null) return false;
+		return true; // tool was found.
+	}
+	
+	/** true if this inv contains the given item. */
 	public boolean hasItem(Item i) {
 		Item ti = this.findItem(i);
 		return ti != null;
 	}
-
+	
+	/** Removes resources from your inventory */
 	public boolean removeResource(Resource r, int count) {
-		ResourceItem ri = findResource(r);
-		if (ri == null) return false;
-		if (ri.count < count) return false;
-		ri.count -= count;
-		if (ri.count <= 0) items.remove(ri);
-		return true;
+		ResourceItem ri = findResource(r); // finds the matching ResourceItem in your inv, if it exists.
+		if (ri == null) return false; // can't remove if you don't have that resource
+		if (ri.count < count) return false; // can't remove if you don't have the given amount of that resource
+		ri.count -= count; // remove the given amount
+		if (ri.count <= 0) items.remove(ri); // remove the ResourceItem from the inv if there are no more of that Resource.
+		return true; // removal successful, at this point.
 	}
-
+	
+	/**  */
 	public boolean removeTool(ToolType t, int level) {
-		ToolItem ti = findtool(t);
+		ToolItem ti = findtool(t, level, false); // find the exact tool.
 		if (ti == null) return false;
-		if (ti.level < level) return false;
-		ti.level -= level;
-		if (ti.level <= 0) items.remove(ti);
+		//if (ti.level < level) return false; //
+		//ti.level -= level;
+		//if (ti.level <= 0) items.remove(ti); // remove if the item was entirely used up.
+		items.remove(ti);
 		return true;
 	}
 	
-	public boolean removeItem(Item i) { // apparently this can only be a lava bucket...
-		BucketLavaItem ri = (BucketLavaItem)this.findItem(i);
-		if(ri == null) {
-			return false;
-		} else {
-			this.items.remove(ri);
-			return true;
-		}
+	/** removes item from this inv. */
+	public boolean removeItem(Item i) { // This was expected to only be a lava bucket, it seems.
+		ResourceItem ri = findItem(i);
+		if(ri == null) return false;
+		else items.remove(ri); // remove the item.
+		
+		return true;
 	}
-
+	
+	/** that gets the score for items...? Really, all it's doing is counting the given resource... */
 	public static int scored(Resource r) {
 		int lscore = 0;
 		ResourceItem ri = findResources(r);
@@ -135,18 +148,19 @@ public class Inventory {
 		}
 		return lscore;
 	}
-
+	
+	/** Returns the how many of an item you have in the inventory */
 	public int count(Item item) {
-		if (item instanceof ResourceItem) {
-			ResourceItem ri = findResource(((ResourceItem) item).resource);
-			if (ri != null) return ri.count;
-		} else {
+		if (item instanceof ResourceItem) { // if the item is a resource...
+			ResourceItem ri = findResource(((ResourceItem) item).resource); // find the ResourceItem in your inv
+			if (ri != null) return ri.count; // if the ResourceItem was found, return the stored amount.
+		} else { // if the item is NOT a resource...
 			int count = 0;
-			for (int i = 0; i < items.size(); i++) {
-				if (items.get(i).matches(item)) count++;
+			for (int i = 0; i < items.size(); i++) { // loop through inv
+				if (items.get(i).matches(item)) count++; // increment count if the current item matches the one given
 			}
-			return count;
+			return count; // return count
 		}
-		return 0;
+		return 0; // should never reach here.
 	}
 }
