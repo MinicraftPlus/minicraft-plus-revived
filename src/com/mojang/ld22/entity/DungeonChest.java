84a9095dc71198d2cc24e75195c2c9d634f2cde7
Christopher Johns <cjohns4288@gmail.com>
2017-04-09 17:06:20 -0700

    simplified inv creation

--- src/com/mojang/ld22/entity/DungeonChest.java
+++ src/com/mojang/ld22/entity/DungeonChest.java
@@ -14,18 +14,24 @@ import java.util.Random;
 
 public class DungeonChest extends Furniture {
 
-	public Inventory inventory = new Inventory();
+	public Inventory inventory;
 	public Random random = new Random();
-	public boolean islocked = true;
+	public boolean islocked;
 	private static int openCol = Color.get(-1, 2, 115, 225);
 	
 	public DungeonChest() {
 		super("Dungeon Chest");
-		getInventory(inventory);
+		populateInv();
 		
+		islocked = true;
 		sprite = 1;
 		col = Color.get(-1, 222, 333, 555);
-		
+		// this is always the case here; locked is set to true above, and canLight() in Entity.java returns false.
+		col0 = Color.get(-1, 111, 222, 444);
+		col1 = Color.get(-1, 222, 333, 555);
+		col2 = Color.get(-1, 111, 222, 444);
+		col3 = Color.get(-1, 0, 111, 333);
+		/*
 		if(canLight()) {
 			if(islocked) // can light, and locked.
 				col0 = col1 = col2 = col3 = col;
@@ -42,17 +48,14 @@ public class DungeonChest extends Furniture {
 			col1 = Color.get(-1, 222, 333, 555);
 			col2 = Color.get(-1, 111, 222, 444);
 			col3 = Color.get(-1, 0, 111, 333);
-		}
+		}*/
 	}
 	
 	public boolean use(Player player, int attackDir) {
 		if(!islocked) {
-			player.game.setMenu(new ContainerMenu(player, "Dungeon Chest", inventory));
+			player.game.setMenu(new ContainerMenu(player, name, inventory));
 			return true;
 		} else {
-			int aw;
-			AirWizard wizard;
-			
 			boolean activeKey = player.activeItem != null && player.activeItem.getName().equals("Key");
 			boolean invKey = player.inventory.hasResources(Resource.key, 1);
 			if(activeKey || invKey) {
@@ -66,19 +69,19 @@ public class DungeonChest extends Furniture {
 				}
 				
 				islocked = false;
-				player.game.setMenu(new ContainerMenu(player, "Dungeon Chest", inventory));
+				player.game.setMenu(new ContainerMenu(player, name, inventory));//use(player, attackDir);
 				col = col0 = col1 = col2 = col3 = openCol;
 				
 				level.add(new SmashParticle(x * 16 + 8, y * 16 + 8));
 				level.add(new TextParticle("-1 key", x, y, Color.get(-1, 500, 500, 500)));
 				level.chestcount--;
 				if(level.chestcount == 0) {
-					for(aw = 0; aw < 5; aw++) {
+					for(int i = 0; i < 5; i++) {
 						level.add(new ItemEntity(new ResourceItem(Resource.goldapple), x, y));
 					}
 					
 					Game.notifications.add("You hear a noise from the surface!");
-					wizard = new AirWizard(true);
+					AirWizard wizard = new AirWizard(true);
 					wizard.x = Game.levels[3].w / 2;
 					wizard.y = Game.levels[3].h / 2;
 					Game.levels[3].add(wizard);
@@ -88,9 +91,61 @@ public class DungeonChest extends Furniture {
 			return true;
 		}
 	}
-
-	public void getInventory(Inventory itemlist) {
-		if(random.nextInt(8) == 1) {
+	
+	/*static int[] rChance = {8, 6, 5, 4, 3, 3, 4, 5, 5, 5, 5, 6, 4, 4, 2, 7, 7, 14, 14, 7, 5};
+	static Resource[] rContents = {Resource.gemarmor, Resource.garmor, Resource.iarmor, Resource.speedpotion, Resource.lightpotion, Resource.potion, Resource.lightpotion, Resource.steak, Resource.cookedpork, Resource.gem, Resource.gem, Resource.speedpotion, Resource.wood, Resource.wool, Resource.coal, Resource.regenpotion, Resource.energypotion, Resource.timepotion, Resource.shieldpotion, Resource.lavapotion, Resource.hastepotion};
+	static int[] rNumber = {1, 1, 2, 2, 2, 10, 3, 6, 6, 7, 8, 5, 20, 12, 4, 1, 1, 1, 1, 1, 3};
+	static int[] tChance = {6, 4, 4, 6, 7, 7};
+	static ToolType[] tContents = {ToolType.claymore, ToolType.sword, ToolType.claymore, ToolType.bow, ToolType.bow, ToolType.sword};
+	static int[] tLevel = {2, 3, 1, 3, 4, 4};
+	*/
+	
+	private void populateInv() {
+		inventory = new Inventory(this);
+		tryAdd(8, Resource.gemarmor, 1);
+		tryAdd(6, Resource.garmor, 1);
+		tryAdd(5, Resource.iarmor, 2);
+		tryAdd(4, Resource.speedpotion, 2);
+		tryAdd(3, Resource.lightpotion, 2);
+		tryAdd(3, Resource.potion, 10);
+		tryAdd(4, Resource.lightpotion, 3);
+		tryAdd(5, Resource.steak, 6);
+		tryAdd(5, Resource.cookedpork, 6);
+		tryAdd(5, Resource.gem, 7);
+		tryAdd(5, Resource.gem, 8);
+		tryAdd(6, Resource.speedpotion, 5);
+		tryAdd(4, Resource.wood, 20);
+		tryAdd(4, Resource.wool, 12);
+		tryAdd(2, Resource.coal, 4);
+		tryAdd(7, Resource.regenpotion, 1);
+		tryAdd(7, Resource.energypotion, 1);
+		tryAdd(14, Resource.timepotion, 1);
+		tryAdd(14, Resource.shieldpotion, 1);
+		tryAdd(7, Resource.lavapotion, 1);
+		tryAdd(5, Resource.hastepotion, 3);
+		
+		tryAdd(6, ToolType.claymore, 2);
+		tryAdd(4, ToolType.sword, 3);
+		tryAdd(4, ToolType.claymore, 1);
+		tryAdd(6, ToolType.bow, 3);
+		tryAdd(7, ToolType.bow, 4);
+		tryAdd(7, ToolType.sword, 4);
+		/*
+		for(int i = 0; i < rContents.length; i++) {
+			if(random.nextInt(rChance[i]) == 1)
+				inventory.add(new ResourceItem(rContents[i], rNumber[i]));
+		}
+		for(int i = 0; i < tContents.length; i++) {
+			if(random.nextInt(tChance[i]) == 1)
+				inventory.add(new ToolItem(tContents[i], tLevel[i]));
+		}
+		*/
+		if(inventory.invSize() < 1) {
+			inventory.add(new ResourceItem(Resource.steak, 6));
+			inventory.add(new ResourceItem(Resource.timepotion, 1));
+			inventory.add(new ToolItem(ToolType.hatchet, 4));
+		}
+		/*if(random.nextInt(8) == 1) {
 			itemlist.add(new ResourceItem(Resource.gemarmor));
 		}
 
@@ -196,14 +251,16 @@ public class DungeonChest extends Furniture {
 
 		if(random.nextInt(5) == 1) {
 			itemlist.add(new ResourceItem(Resource.hastepotion, 3));
-		}
-
-		if(inventory.items.size() < 1) {
-			inventory.items.add(new ResourceItem(Resource.steak, 6));
-			inventory.items.add(new ResourceItem(Resource.timepotion, 1));
-			inventory.items.add(new ToolItem(ToolType.hatchet, 4));
-		}
-
+		}*/
+	}
+	
+	private void tryAdd(int chance, Resource item, int num) {
+		if(random.nextInt(chance) == 1)
+			inventory.add(new ResourceItem(item, num));
+	}
+	private void tryAdd(int chance, ToolType type, int lvl) {
+		if(random.nextInt(chance) == 1)
+			inventory.add(new ToolItem(type, lvl));
 	}
 
 	protected void touchedBy(Entity entity) {
