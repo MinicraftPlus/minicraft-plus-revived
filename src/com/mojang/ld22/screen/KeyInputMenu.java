595fb8615fe03f6a84d10eadc50103eebbbc02c3
Christopher Johns <cjohns4288@gmail.com>
2017-04-25 16:08:12 -0700

    added KeyInputMenu.java placeholder for editing key controls

---
+++ src/com/mojang/ld22/screen/KeyInputMenu.java
@@ -0,0 +1,20 @@
+package com.mojang.ld22.screen;
+
+import com.mojang.ld22.InputHandler;
+
+public class KeyInputMenu {
+	
+	// store a reference to InputHandler's Key HashMap, one way or another.
+	
+	public KeyInputMenu() {
+		
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+	}
+}
