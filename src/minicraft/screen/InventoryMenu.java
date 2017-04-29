bd7f78577dc12428c0eb9e24cd826b05b5271f99
Christopher Johns <cjohns4288@gmail.com>
2017-04-28 17:25:21 -0700

    successfully implemented ScrollingMenu as parent class

--- src/minicraft/screen/InventoryMenu.java
+++ src/minicraft/screen/InventoryMenu.java
@@ -1,27 +1,37 @@
 package minicraft.screen;
 
 import minicraft.entity.Player;
+import minicraft.gfx.Color;
 import minicraft.gfx.Font;
 import minicraft.gfx.Screen;
 import minicraft.sound.Sound;
 import minicraft.item.ResourceItem;
+import minicraft.item.Item;
+import java.util.List;
+import java.util.Arrays;
 
-public class InventoryMenu extends Menu {
+public class InventoryMenu extends ScrollingMenu {
 	private Player player;
-	private int selected = 0;
+	//private int selected = 0;
 
 	public InventoryMenu(Player player) {
+		super(player.inventory.getItemNames(), 9, 2*8, 2*8, 0, Color.get(-1, 555), Color.get(-1, 555));
 		this.player = player;
 		
 		if (player.activeItem != null) { // If the player has an active item, then...
 			player.inventory.add(0, player.activeItem); // that active item will go into the inventory
 			player.activeItem = null; // the player will not have an active item anymore.
+			options = player.inventory.getItemNames();
+		}
+		for(int i = 0; i < options.length; i++) {
+			options[i] = " "+options[i];
 		}
 	}
 
 	public void tick() {
 		if (input.getKey("menu").clicked) game.setMenu(null);
-
+		
+		super.tick();/*
 		if (input.getKey("up").clicked) selected--;
 		if (input.getKey("down").clicked) selected++;
 		if (input.getKey("up").clicked) Sound.pickup.play();
@@ -31,8 +41,8 @@ public class InventoryMenu extends Menu {
 		if (len == 0) selected = 0;
 		if (selected < 0) selected += len;
 		if (selected >= len) selected -= len;
-		
-		if (input.getKey("attack").clicked && len > 0) { // If your inventory is not empty, and the player presses the "Attack" key...
+		*/
+		if (input.getKey("attack").clicked && options.length > 0) { // If your inventory is not empty, and the player presses the "Attack" key...
 			player.activeItem = player.inventory.remove(selected); // The item will be removed from the inventory, and placed as the player's active item.
 			game.setMenu(null); // the game will go back to the gameplay
 		}
@@ -40,6 +50,10 @@ public class InventoryMenu extends Menu {
 
 	public void render(Screen screen) {
 		renderFrame(screen, "inventory", 1, 1, 22, 11); // renders the blue box for the inventory
-		renderItemList(screen, 1, 1, 22, 11, player.inventory.getItems(), selected); // renders the icon's and names of all the items in your inventory.
+		super.render(screen);
+		List<Item> items = player.inventory.getItems();
+		for(int i = 0; i < dispSize; i++)
+			items.get(offset+i).renderIcon(screen, 2*8, 8*(2+i));
+		//renderItemList(screen, 1, 1, 22, 11, player.inventory.getItems(), selected); // renders the icon's and names of all the items in your inventory.
 	}
 }
