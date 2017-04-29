77543d0d2e07362ba65e8fa649b38120d8bcce7b
Christopher Johns <cjohns4288@gmail.com>
2017-04-28 17:24:24 -0700

    revised with renderAs, easier to change menu temporarily

--- src/minicraft/screen/SelectMenu.java
+++ src/minicraft/screen/SelectMenu.java
@@ -8,25 +8,28 @@ import minicraft.sound.Sound;
 
 public abstract class SelectMenu extends Menu {
 	
-	protected String[] choices;
+	protected String[] options;
 	protected int selected;
 	
 	protected int x, y, spacing;
 	protected boolean centered;
 	
-	public SelectMenu(String[] choices, int x, int y, int spacing) {
-		this.choices = choices;
+	private int colSelect, colOther;
+	
+	protected SelectMenu(String[] options, int x, int y, boolean center, int spacing, int col1, int col2) {
+		this.options = options;
 		this.x = x;
 		this.y = y;
 		this.spacing = spacing;
-		centered = false;
+		centered = center;
+		colSelect = col1;
+		colOther = col2;
 	}
-	public SelectMenu(String[] choices, int y, int spacing) {
-		this.choices = choices;
-		x = 0; // this doesn't matter, it won't be used.
-		this.y = y;
-		this.spacing = spacing;
-		centered = true;
+	protected SelectMenu(String[] options, int x, int y, int spacing, int col1, int col2) {
+		this(options, x, y, false, spacing, col1, col2);
+	}
+	protected SelectMenu(String[] options, int y, int spacing, int col1, int col2) {
+		this(options, 0, y, true, spacing, col1, col2);
 	}
 	
 	public void tick() {
@@ -35,21 +38,27 @@ public abstract class SelectMenu extends Menu {
 		if(input.getKey("up").clicked) selected--;
 		if(input.getKey("down").clicked) selected++;
 		
-		if(selected >= choices.length) selected = 0;
-		if(selected < 0) selected = choices.length - 1;
+		if(selected >= options.length) selected = 0;
+		if(selected < 0) selected = options.length - 1;
 		
 		if(prevSel != selected) Sound.craft.play();
 	}
 	
 	public void render(Screen screen) {
-		for(int i = 0; i < choices.length; i++) {
-			boolean current = selected == i;
-			int col = (current ? Color.get(-1, 555) : Color.get(-1, 111));
-			String option = (current ? "> "+choices[i]+" <" : choices[i]);
-			if (centered) Font.drawCentered(option, screen, y + i*spacing, col);
+		renderAs(screen, this.options, selected);
+	}
+	
+	protected void renderAs(Screen screen, String[] options, int sel) {
+		for(int i = 0; i < options.length; i++) {
+			if(options[i] == null) continue;
+			boolean current = sel == i;
+			int col = (current ? colSelect : colOther);
+			String option = (current ? "> "+options[i]+" <" : options[i]);
+			int ypos = y + i*(spacing+Font.textHeight());
+			if (centered) Font.drawCentered(option, screen, ypos, col);
 			else {
 				int x = current ? this.x-Font.textWidth("> ") : this.x;
-				Font.draw(option, screen, x, y + i*spacing, col);
+				Font.draw(option, screen, x, ypos, col);
 			}
 		}
 	}
