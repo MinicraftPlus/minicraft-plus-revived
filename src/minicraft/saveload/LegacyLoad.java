2bea4892a4fa4492902b0e695cf672551e28a752
Christopher Johns <cjohns4288@gmail.com>
2017-05-02 22:15:40 -0700

    moved old Load features to LegacyLoad.java

--- src/minicraft/saveload/LegacyLoad.java
+++ src/minicraft/saveload/LegacyLoad.java
@@ -48,9 +48,8 @@ import minicraft.screen.LoadingMenu;
 import minicraft.screen.ModeMenu;
 import minicraft.screen.OptionsMenu;
 
-
-// I may want to consider a "LegacyLoad class in the near future, simply to reduce the clutter and really allow me to "start fresh". :)
-public class Load {
+/// this class is simply a way to seperate all the old, compatibility complications into a seperate file.
+public class LegacyLoad {
 	
 	String location = Game.gameDir;
 	File folder;
@@ -61,100 +60,32 @@ public class Load {
 	ArrayList<String> extradata;
 	
 	public boolean hasloadedbigworldalready;
-	Version currentVer, worldVer;
-	boolean oldSave = false, hasGlobalPrefs = false;
+	Load.Version currentVer, worldVer;
+	boolean oldSave = false;
 	
-	private Load() {
-		currentVer = new Version(Game.VERSION);
+	public LegacyLoad(Game game, String worldname) {
+		currentVer = new Load.Version(Game.VERSION);
 		worldVer = null;
-		
-		File testFile = new File(location + "/Preferences" + extention);
-		hasGlobalPrefs = testFile.exists();
 		
 		data = new ArrayList<String>();
 		extradata = new ArrayList<String>();
 		hasloadedbigworldalready = false;
-	}
-	
-	public Load(Game game, String worldname) {
-		this();
-		if(!hasGlobalPrefs)
-			new LegacyLoad(game, worldname);
-		else {
-			location += "/saves/" + worldname + "/";
-			
-			loadGame("Game", game); // more of the version will be determined here
-			loadWorld("Level");
-			loadPlayer("Player", game.player);
-			loadInventory("Inventory", game.player.inventory);
-			loadEntities("Entities", game.player);
-			LoadingMenu.percentage = 0; // reset
-		}
-	}
-	
-	public Load(Game game) {
-		this();
 		
-		location += "/";
+		location += "/saves/" + worldname + "/";
 		
-		if(hasGlobalPrefs)
-			loadPrefs("Preferences", game);
-	}
-	
-	protected static class Version implements Comparable {
-		public Integer make, major, minor, dev;
+		File testFile = new File(location + "KeyPrefs" + extention);
+		if(!testFile.exists()) {
+			worldVer = new Load.Version("1.8");
+			oldSave = true;
+		} else
+			testFile.delete(); // we don't care about it anymore anyway.
 		
-		public Version(String version) {
-			String[] nums = version.split("\\.");
-			try {
-				if(nums.length > 0) make = Integer.parseInt(nums[0]);
-				else make = 0;
-				
-				if(nums.length > 1) major = Integer.parseInt(nums[1]);
-				else major = 0;
-				
-				String min;
-				if(nums.length > 2) min = nums[2];
-				else min = "";
-				
-				if(min.contains("-")) {
-					String[] mindev = min.split("-");
-					minor = Integer.parseInt(mindev[0]);
-					dev = Integer.parseInt(mindev[1].replace("pre", "").replace("dev", ""));
-				} else {
-					if(!min.equals("")) minor = Integer.parseInt(min);
-					else minor = 0;
-					dev = 0;
-				}
-			} catch(NumberFormatException ex) {
-				System.out.println("INVALID version number: " + version);
-			} catch(Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-		
-		// the returned value of this method (-1, 0, or 1) is determined by whether this object is less than, equal to, or greater than the specified object.
-		public int compareTo(Object other) throws NullPointerException, ClassCastException {
-			if(other == null) throw new NullPointerException();
-			if(other instanceof Version == false) { // if the passed object is not a Version...
-				throw new ClassCastException("Cannot compare type Version with type " + other.getClass().getTypeName());
-			}
-			Version ov = (Version)other;
-			
-			if(make != ov.make) return make.compareTo(ov.make);
-			else if(major != ov.major) return major.compareTo(ov.major);
-			else if(minor != ov.minor) return minor.compareTo(ov.minor);
-			else if(dev != ov.dev) {
-				if(dev == 0) return 1; //0 is the last "dev" version, as it is not a dev.
-				else if(ov.dev == 0) return -1;
-				else return dev.compareTo(ov.dev);
-			}
-			else return 0; // the versions are equal.
-		}
-		
-		public String toString() {
-			return make + "." + major + "." + minor + (dev == 0 ? "" : "-dev" + dev);
-		}
+		loadGame("Game", game); // more of the version will be determined here
+		loadWorld("Level");
+		loadPlayer("Player", game.player);
+		loadInventory("Inventory", game.player.inventory);
+		loadEntities("Entities", game.player);
+		LoadingMenu.percentage = 0; // reset
 	}
 	
 	public void loadFromFile(String filename) {
@@ -205,34 +136,41 @@ public class Load {
 	
 	public void loadGame(String filename, Game game) {
 		loadFromFile(location + filename + extention);
-		
-		worldVer = new Version(data.get(0)); // gets the world version
-		Game.setTime(Integer.parseInt(data.get(1)));
-		
-		if(worldVer.compareTo(new Version("1.9.3-dev2")) >= 0) {
-			game.gameTime = Integer.parseInt(data.get(2));
-			Game.pastDay1 = game.gameTime > 65000;
-		} else {
+		boolean hasVersion = data.get(0).contains(".");
+		if(hasVersion) {
+			worldVer = new Load.Version(data.get(0)); // gets the world version
+			Game.setTime(Integer.parseInt(data.get(1)));
 			Game.astime = Integer.parseInt(data.get(2));
 			game.gameTime = 65000; // prevents time cheating.
+			
+			if(worldVer.compareTo(new Load.Version("1.9.2")) < 0) {
+				OptionsMenu.autosave = Boolean.parseBoolean(data.get(3));
+				OptionsMenu.isSoundAct = Boolean.parseBoolean(data.get(4));
+				if(worldVer.compareTo(new Load.Version("1.9.2-dev2")) >= 0)
+					AirWizard.beaten = Boolean.parseBoolean(data.get(5));
+			} else { // this is 1.9.2 official or after
+				OptionsMenu.diff = Integer.parseInt(data.get(3));
+				AirWizard.beaten = Boolean.parseBoolean(data.get(4));
+			}
 		}
-		
-		OptionsMenu.diff = Integer.parseInt(data.get(3));
-		AirWizard.beaten = Boolean.parseBoolean(data.get(4));
-	}
-	
-	public void loadPrefs(String filename, Game game) {
-		loadFromFile(location + filename + extention);
-		
-		OptionsMenu.isSoundAct = Boolean.parseBoolean(data.get(0));
-		OptionsMenu.autosave = Boolean.parseBoolean(data.get(1));
-		
-		List<String> subdata = data.subList(2, data.size());
-		
-		Iterator<String> keys = subdata.iterator();
-		while(keys.hasNext()) {
-			String[] map = keys.next().split(";");
-			game.input.setKey(map[0], map[1]);
+		else {
+			if(data.size() == 5) {
+				worldVer = new Load.Version("1.9");
+				Game.setTime(Integer.parseInt(data.get(0)));
+				Game.astime = Integer.parseInt(data.get(1));
+				OptionsMenu.autosave = Boolean.parseBoolean(data.get(3));
+				OptionsMenu.isSoundAct = Boolean.parseBoolean(data.get(4));
+			} else { // version == 1.8?
+				if(!oldSave) {
+					System.out.println("UNEXPECTED WORLD VERSION");
+					worldVer = new Load.Version("1.8.1");
+				}
+				// for backwards compatibility
+				Game.tickCount = Integer.parseInt(data.get(0));
+				Game.astime = Integer.parseInt(data.get(1));
+				game.player.ac = Integer.parseInt(data.get(3));
+				OptionsMenu.autosave = false;
+			}
 		}
 	}
 	
@@ -262,14 +200,22 @@ public class Load {
 		player.armor = Integer.parseInt(data.get(5));
 		
 		String modedata;
-		if(player.armor > 0) {
-			player.armorDamageBuffer = Integer.parseInt(data.get(13));
-			player.curArmor = (ArmorResource)(((ResourceItem)ListItems.getItem(data.get(14))).resource);
+		if(!oldSave) {
+			if(data.size() >= 14) {
+				if(worldVer == null) worldVer = new Load.Version("1.9.1-pre1");
+				player.armorDamageBuffer = Integer.parseInt(data.get(13));
+				player.curArmor = (ArmorResource)(((ResourceItem)ListItems.getItem(data.get(14))).resource);
+			} else player.armor = 0;
+			
+			player.ac = Integer.parseInt(data.get(7));
+			Game.currentLevel = Integer.parseInt(data.get(8));
+			modedata = data.get(9);
+			
+		} else {
+			// old, 1.8 save.
+			Game.currentLevel = Integer.parseInt(data.get(7));
+			modedata = data.get(8);
 		}
-		
-		player.ac = Integer.parseInt(data.get(7));
-		Game.currentLevel = Integer.parseInt(data.get(8));
-		modedata = data.get(9);
 		
 		Player.score = Integer.parseInt(data.get(6));
 		Game.levels[Game.currentLevel].add(player);
@@ -287,23 +233,33 @@ public class Load {
 		
 		ModeMenu.updateModeBools(mode);
 		
-		if(!data.get(10).equals("PotionEffects[]")) {
-			String[] effects = data.get(10).replace("PotionEffects[", "").replace("]", "").split(":");
+		boolean hasEffects;
+		int potionIdx = 10;
+		if(oldSave) {
+			hasEffects = data.size() > 10 && data.get(data.size()-2).contains("PotionEffects[");
+			potionIdx = data.size() - 2;
+		} else
+			hasEffects = !data.get(10).equals("PotionEffects[]"); // newer save
+		
+		if(hasEffects) {
+			String[] effects = data.get(potionIdx).replace("PotionEffects[", "").replace("]", "").split(":");
 			
 			for(int i = 0; i < effects.length; i++) {
 				String[] effect = effects[i].split(";");
 				String pName = effect[0];
+				if(oldSave) pName = pName.replace("P.", "Potion");
 				PotionResource.applyPotion(player, pName, Integer.parseInt(effect[1]));
 			}
 		}
 		
-		String colors = data.get(11).replace("[", "").replace("]", "");
+		String colors = data.get(oldSave?data.size()-1:11).replace("[", "").replace("]", "");
 		String[] color = colors.split(";");
 		player.r = Integer.parseInt(color[0]);
 		player.g = Integer.parseInt(color[1]);
 		player.b = Integer.parseInt(color[2]);
 		
-		Player.skinon = Boolean.parseBoolean(data.get(12));
+		if(!oldSave) Player.skinon = Boolean.parseBoolean(data.get(12));
+		else Player.skinon = false;
 	}
 	
 	public void loadInventory(String filename, Inventory inventory) {
@@ -314,8 +270,10 @@ public class Load {
 			String item = data.get(i);
 			
 			if(ListItems.getItem(item) instanceof ResourceItem) {
+				if(oldSave && i == 0) item = item.replace(";0", ";1");
 				List<String> curData = Arrays.asList(item.split(";"));
 				String itemName = curData.get(0);
+				if(oldSave) itemName = subOldName(itemName);
 				
 				Item newItem = ListItems.getItem(itemName);
 				
@@ -328,10 +286,18 @@ public class Load {
 					}
 				}
 			} else {
+				if(oldSave) item = subOldName(item);
 				Item toAdd = ListItems.getItem(item);
 				inventory.add(toAdd);
 			}
 		}
+	}
+	
+	private String subOldName(String oldName) {
+		//System.out.println("old name: " + oldName);
+		String newName = oldName.replace("P.", "Potion").replace("Fish Rod", "Fishing Rod").replace("bed", "Bed");
+		//System.out.println("new name: " + newName);
+		return newName;
 	}
 	
 	public void loadEntities(String filename, Player player) {
@@ -344,7 +310,7 @@ public class Load {
 		for(int i = 0; i < data.size(); i++) {
 			List<String> info = Arrays.asList(data.get(i).substring(data.get(i).indexOf("[") + 1, data.get(i).indexOf("]")).split(":")); // this gets everything inside the "[...]" after the entity name.
 			
-			String entityName = data.get(i).substring(0, data.get(i).indexOf("[")); // this gets the text before "[", which is the entity name.
+			String entityName = data.get(i).substring(0, data.get(i).indexOf("[")).replace("bed", "Bed").replace("II", ""); // this gets the text before "[", which is the entity name.
 			int x = Integer.parseInt(info.get(0));
 			int y = Integer.parseInt(info.get(1));
 			
@@ -372,6 +338,9 @@ public class Load {
 					int endIdx = chestInfo.size()-(isDeathChest||isDungeonChest?1:0);
 					for(int idx = 0; idx < endIdx; idx++) {
 						String itemData = chestInfo.get(idx);
+						if(worldVer.compareTo(new Load.Version("1.9.1")) < 0) // if this world is before 1.9.1
+							if(itemData.equals("")) continue; // this skips any null items
+						if(oldSave) itemData = subOldName(itemData);
 						Item item = ListItems.getItem(itemData);
 						if (item instanceof ResourceItem) {
 							String[] resourceData = (itemData + ";1").split(";"); // this appends ";1" to the end, meaning one item, to everything; but if it was already there, then it becomes the 3rd element in the list, which is ignored.
@@ -384,7 +353,7 @@ public class Load {
 					}
 					
 					if (isDeathChest) {
-						((DeathChest)chest).time = Integer.parseInt(chestInfo.get(chestInfo.size()-1));
+						((DeathChest)chest).time = Integer.parseInt(chestInfo.get(chestInfo.size()-1).replace("tl;", "")); // "tl;" is only for old save support
 					} else if (isDungeonChest) {
 						((DungeonChest)chest).isLocked = Boolean.parseBoolean(chestInfo.get(chestInfo.size()-1));
 					}
@@ -437,7 +406,7 @@ public class Load {
 			case "IronLantern": return (Entity)(new IronLantern());
 			case "GoldLantern": return (Entity)(new GoldLantern());
 			//case "Spark": return (Entity)(new Spark());
-			default : /*if(Game.debug)*/ System.out.println("LOAD: unknown or outdated entity requested: " + string);
+			default : /*if(Game.debug)*/ System.out.println("LEGACYLOAD: unknown or outdated entity requested: " + string);
 				return null;
 		}
 	}
