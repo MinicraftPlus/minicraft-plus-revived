98934121bf50ebc3034c65559748040dafe489ab
Christopher Johns <cjohns4288@gmail.com>
2017-05-05 21:46:09 -0700

    added color range correction to tint method

--- src/minicraft/gfx/Color.java
+++ src/minicraft/gfx/Color.java
@@ -72,6 +72,9 @@ public class Color {
 		int b = color % 6;
 		r+=amount; g+=amount; b+=amount;
 		
+		if(r<0) r = 0; if(g<0) g = 0; if(b<0) b = 0;
+		if(r>255) r = 255; if(g>255) g = 255; if(b>255) b = 255;
+		
 		return r * 36 + g * 6 + b;
 	}
 	
