85d76a26518a407a0cd7f3ca4b2b00f0d66573fb
Christopher Johns <cjohns4288@gmail.com>
2017-05-07 17:11:39 -0700

    upped version to 1.9.4-dev2

--- src/minicraft/Game.java
+++ src/minicraft/Game.java
@@ -55,7 +55,7 @@ public class Game extends Canvas implements Runnable {
 	/// MANAGERIAL VARS AND RUNNING
 	
 	public static final String NAME = "Minicraft Plus"; // This is the name on the application window
-	public static final String VERSION = "1.9.4-dev1";
+	public static final String VERSION = "1.9.4-dev2";
 	public static final int HEIGHT = 192;
 	public static final int WIDTH = 288;
 	private static float SCALE = 3;
