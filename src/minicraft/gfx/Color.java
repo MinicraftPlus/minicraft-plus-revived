7bd3b724bba665d308e6ac02dd34f55b3ba43196
Christopher Johns <cjohns4288@gmail.com>
2017-05-07 19:29:19 -0700

    created parallel system for 24bit color

--- src/minicraft/gfx/Color.java
+++ src/minicraft/gfx/Color.java
@@ -1,5 +1,7 @@
 package minicraft.gfx;
 
+//import java.awt.Color;
+
 public class Color {
 	
 	/* To explain this class, you have to know how Bit-Shifting works.
@@ -24,22 +26,40 @@ public class Color {
 			and white is the fourth color.
 	*/
 	
+	/**
+		To provide a method to the madness, all methods ending in "Color" are copies of the methods of the same name without the "Color" part, execpt they convert the given value to 24bit Java RGB rather than the normal, minicraft 8-bit total RGB (that really only goes up to 216).
+	*/
+	
 	/** This returns a integer with 4 rgb color values. */
 	public static int get(int a, int b, int c, int d) {
-		//"get()"s each value, and shifts each one's bit to the left the specified number of times
-		return (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a)); // this seems like far too many bits for how few colors there are in minicraft...
+		//converts each color to 8-bit, and shifts each one 8-bits to the left a certain number of times, to create 4 colors set one after the other.
+		// if r,g,b were 8 bits each, these would have to be shifted 72, 48, 24. The number would be 2^98; far too big for an int. Heck, this number is barely small enough, being... 2^32. wait, that's bigger than 2^31-1!
+		return (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a));
 	}
 	public static int get(int a, int bcd) {
 		return get(a, bcd, bcd, bcd); // just a shortcut.
 	}
 	//public static int get(int abcd) {return get(abcd, abcd, abcd, abcd);}
 	
+	/** converts a 0-5 scale rgb color to a 24-bit color, r,g,b are each 8 bits. Used 4 times in 4 param. get() method, for each color. */
+	public static int get(int d) {
+		if (d < 0) return 255; // if d is smaller than 0, then return 255.
+		int r = d / 100 % 10; // the red value is the remainder of (d/100) / 10
+		int g = d / 10 % 10; // the green value is the remainder of (d/10) / 10
+		int b = d % 10; // the blue value is the remainder of d / 10.
+		
+		return r * 36 + g * 6 + b; // returns (red value * 36) + (green value * 6) + (blue value)
+		
+		// Why do we need all this math to get the colors? I don't even know. -David
+		// This is just converting minicraft colors, which can only go 0 to 5, to regular, 8-bit rgb colors, which go from 0 to 255. Though, these add up to 216, max... -Chris J
+	}
+	
 	/*//similar to get(), it looks like, but just one value..?
 	public static int pixel(int a) {
 		return (get(a) << 24) + (get(a) << 16) + (get(a) << 8) + get(a);
 	}*/
 	
-	// this makes an int that you would pass to the get method, from seperate rgb values.
+	// this makes an int that you would pass to the get(a,b,c,d), or get(d), method, from three seperate 8-bit r,g,b values.
 	public static int rgb(int red, int green, int blue) {
 		if (red > 255) red = 255;
 		if (green > 255) green = 255;
@@ -53,34 +73,21 @@ public class Color {
 		return rgb;
 	}
 	
-	/** gets the color to use based off an integer; used 4 times in 4 param. method, for each color. */
-	public static int get(int d) {
-		if (d < 0) return 255; // if d is smaller than 0, then return 255.
-		int r = d / 100 % 10; // the red value is the remainder of (d/100) / 10
-		int g = d / 10 % 10; // the green value is the remainder of (d/10) / 10
-		int b = d % 10; // the blue value is the remainder of d / 10.
-		return r * 36 + g * 6 + b; // returns (red value * 36) + (green value * 6) + (blue value)
-		
-		// Why do we need all this math to get the colors? I don't even know. -David
-		// OH! This is just converting minicraft colors, which can only go 0 to 5, to regular, 8-bit rgb colors, which go from 0 to 255! or... wait, actually, maybe not..? They add up to 216, max... -Chris J
-	}
-	
-	/** This method darkens or lightens a pixel by the specified amount. */
+	/** This method darkens or lightens a color (0 to 216 value) by the specified amount. */
 	public static int tint(int color, int amount, boolean isSpriteCol) {
 		if(isSpriteCol) {
-			int[] colors = seperateEncodedSprite(color);
+			int[] colors = seperateEncodedSprite(color); // this just seperates the four 8-bit sprite colors; they are still in base-6 added form.
 			for(int i = 0; i < colors.length; i++) {
-				if(colors[i] == -1) continue;
 				colors[i] = tint(colors[i], amount);
-				colors[i] = decodeRGB(colors[i]);
 			}
-			return get(colors[0], colors[1], colors[2], colors[3]);
+			return (colors[0] << 24) + (colors[1] << 16) + (colors[2] << 8) + colors[3];
 		} else {
-			color = tint(color, amount);
-			return get(color);
+			return tint(color, amount);
 		}
 	}
 	private static int tint(int rgb, int amount) {
+		if(rgb == -1) return -1;
+		
 		int r = (rgb / 36) % 6;
 		int g = (rgb / 6) % 6;
 		int b = rgb % 6;
@@ -109,11 +116,71 @@ public class Color {
 		return cols;*/
 	}
 	/** This turns a 216 scale rgb int into a 0-5 scale "concatenated" rgb int. */
-	public static int decodeRGB(int rgb) {
+	public static int[] decodeRGB(int rgb) {
 		int r = (rgb / 36) % 6;
 		int g = (rgb / 6) % 6;
 		int b = rgb % 6;
-		return rgb(r, g, b);
+		return new int[] {r, g, b};
+	}
+	
+	/// this turns a 0-216 combined minicraft color into a 24-bit r,g,b color.
+	protected static int upgrade(int col) {
+		int[] rgbs = decodeRGB(col);
+		int newcol = rgbs[0]*100 + rgbs[1]*10 + rgbs[2];
+		return getColor(newcol);
+	}
+	
+	/// this takes a 24 bit color, and turns it into an 8-bit color minicraft sprites use.
+	protected static int downgrade(int col) {
+		int[] comps = decodeRGBColor(col);
+		return rgb(comps[0], comps[1], comps[2]);
+	}
+	
+	/// this is like get(d), above, but it returns a 24 bit color, with r,g,b each taking 8 bits, rather than them all taking 8 bits together.
+	protected static int getColor(int d) {
+		int r = d / 100 % 10; // the red value is the remainder of (d/100) / 10
+		int g = d / 10 % 10; // the green value is the remainder of (d/10) / 10
+		int b = d % 10; // the blue value is the remainder of d / 10.
+		
+		/// this is taken from Game.java when it's making the colors. I think the end result is a 0-5 r, g, or b turning into a 0-255 r, g, or b.
+		int rr = (r * 255 / 5);
+		int gg = (g * 255 / 5);
+		int bb = (b * 255 / 5);
+		int mid = (rr * 30 + gg * 59 + bb * 11) / 100;
+		
+		int r1 = ((rr + mid * 1) / 2) * 230 / 255 + 10;
+		int g1 = ((gg + mid * 1) / 2) * 230 / 255 + 10;
+		int b1 = ((bb + mid * 1) / 2) * 230 / 255 + 10;
+		return r1 << 16 | g1 << 8 | b1; // wow, this would work... it's like:
+		/*
+			pattern 1: 00101010 00000000 00000000
+			pattern 2: 00000000 01001110 00000000
+			pattern 3: 00000000 00000000 11011101
+			OR'ing those three bit patterns just kind of joins them.
+		*/
+	}
+	
+	protected static int tintColor(int rgb, int amount) {
+		if(rgb == -1) return -1;
+		
+		int[] comps = decodeRGBColor(rgb);
+		int r = comps[0], g = comps[1], b = comps[2];
+		
+		r+=amount; g+=amount; b+=amount;
+		
+		if(r<0) r = 0; if(g<0) g = 0; if(b<0) b = 0;
+		if(r>255) r = 255; if(g>255) g = 255; if(b>255) b = 255;
+		
+		return r << 16 | g << 8 | b;
+	}
+	
+	protected static int[] decodeRGBColor(int col) {
+		int r = col >> 16;
+		int rp = r << 16;
+		int g = (col - rp) >> 8;
+		int b = col-rp-(g<<8);
+		
+		return new int[] {r, g, b};
 	}
 	
 	/// this is for color testing.
