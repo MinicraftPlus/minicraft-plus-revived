f8536c7acbff42733c84b3c4453e37c68ae2a8cc
Christopher Johns <cjohns4288@gmail.com>
2017-05-08 22:13:22 -0700

    removed refs to LightTiles and TorchTiles; no longer needed (plus a small reversion compatibility fix)

--- src/minicraft/level/tile/Tile.java
+++ src/minicraft/level/tile/Tile.java
@@ -11,10 +11,6 @@ import minicraft.item.resource.Resource;
 import minicraft.level.Level;
 
 public class Tile {
-	public enum OreType {
-        IRON, LAPIS, GOLD, GEM
-    }
-	
 	public static int tickCount = 0; //A global tickCount used in the Lava & water tiles.
 	protected Random random = new Random();
 	
@@ -62,52 +58,10 @@ public class Tile {
 	public static Tile odo = new ObsidianDoorOpenTile(123);
 	public static Tile hardRock = new HardRockTile(18);
 	
-	// light/torch versions; ALL are ONLY used to render torch effects. Don't worry, I can fix that. ;)
-	public static Tile lightgrass = new LightTile(100, grass, 0);
-	public static Tile lightsand = new LightTile(101, sand, 1);
-	public static Tile lighttree = new LightTile(102, tree, 2);
-	public static Tile lightcac = new LightTile(103, cactus, 3);
-	public static Tile lightwater = new LightTile(104, water, 4);
-	public static Tile lightdirt = new LightTile(105, dirt, 5);
-	public static Tile lightflower = new LightTile(107, flower, 6);
-	public static Tile lightstairsUp = new LightTile(108, stairsUp, 7);
-	public static Tile lightstairsDown = new LightTile(109, stairsDown, 8);
-	public static Tile lightplank = new LightTile(110, plank, 9);
-	public static Tile lightsbrick = new LightTile(111, sbrick, 10);
-	public static Tile lwdo = new LightTile(112, wdo, 11);
-	public static Tile lwdc = new LightTile(113, wdc, 12);
-	public static Tile lsdo = new LightTile(114, sdo, 13);
-	public static Tile lsdc = new LightTile(115, sdc, 14);
-	public static Tile lodo = new LightTile(116, odo, 15);
-	public static Tile lodc = new LightTile(117, odc, 16);
-	public static Tile lighthole = new LightTile(119, hole, 17);
-	public static Tile lightwool = new LightTile(57, wool, 18);
-	public static Tile lightrwool = new LightTile(58, redwool, 19);
-	public static Tile lightbwool = new LightTile(59, bluewool, 20);
-	public static Tile lightgwool = new LightTile(60, greenwool, 21);
-	public static Tile lightywool = new LightTile(61, yellowwool, 22);
-	public static Tile lightblwool = new LightTile(62, blackwool, 23);
-	public static Tile lighto = new LightTile(63, o, 24);
-	public static Tile lightts = new LightTile(64, treeSapling, 25);
-	public static Tile lightcs = new LightTile(65, cactusSapling, 26);
-
-	public static Tile torchgrass = new TorchTile(44, lightgrass);
-	public static Tile torchsand = new TorchTile(40, lightsand);
-	public static Tile torchdirt = new TorchTile(46, lightdirt);
-	public static Tile torchplank = new TorchTile(47, lightplank);
-	public static Tile torchsbrick = new TorchTile(48, lightsbrick);
-	public static Tile torchlo = new TorchTile(49, lighto);
-	public static Tile torchwool = new TorchTile(50, wool);
-	public static Tile torchwoolred = new TorchTile(51, lightrwool);
-	public static Tile torchwoolblue = new TorchTile(52, lightbwool);
-	public static Tile torchwoolgreen = new TorchTile(53, lightgwool);
-	public static Tile torchwoolyellow = new TorchTile(54, lightywool);
-	public static Tile torchwoolblack = new TorchTile(55, lightblwool);
-
-	public static Tile ironOre = new OreTile(19, OreType.IRON, Color.get(-1, 100, 322, 544));
-	public static Tile lapisOre = new OreTile(24, OreType.LAPIS, Color.get(-1, 005, 115, 115));
-	public static Tile goldOre = new OreTile(20, OreType.GOLD, Color.get(-1, 110, 440, 553));
-	public static Tile gemOre = new OreTile(21, OreType.GEM, Color.get(-1, 101, 404, 545));
+	public static Tile ironOre = new OreTile(19, OreTile.OreType.IRON, Color.get(-1, 100, 322, 544));
+	public static Tile lapisOre = new OreTile(24, OreTile.OreType.LAPIS, Color.get(-1, 005, 115, 115));
+	public static Tile goldOre = new OreTile(20, OreTile.OreType.GOLD, Color.get(-1, 110, 440, 553));
+	public static Tile gemOre = new OreTile(21, OreTile.OreType.GEM, Color.get(-1, 101, 404, 545));
 	public static Tile cloudCactus = new CloudCactusTile(22); // "ore" in the sky.
 	public static Tile infiniteFall = new InfiniteFallTile(16); // Air tile in the sky.
 
@@ -153,16 +107,13 @@ public class Tile {
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {}
 	
 	/** What happens when you run into the tile (ex: run into a cactus) */
-	public void bumpedInto(Level level, int xt, int yt, Entity entity) {
-	}
+	public void bumpedInto(Level level, int xt, int yt, Entity entity) {}
 	
 	/** Update method */
-	public void tick(Level level, int xt, int yt) {
-	}
+	public void tick(Level level, int xt, int yt) {}
 	
 	/** What happens when you are inside the tile (ex: lava) */
-	public void steppedOn(Level level, int xt, int yt, Entity entity) {
-	}
+	public void steppedOn(Level level, int xt, int yt, Entity entity) {}
 	
 	/** What happens when you hit an item on a tile (ex: Pickaxe on rock) */
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
