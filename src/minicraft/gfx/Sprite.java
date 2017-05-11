55a3b01c1343fd5a4a78d5bb7e36fba8d17e50cd
Christopher Johns <cjohns4288@gmail.com>
2017-05-11 15:27:36 -0700

    added color to sprite + ease-of-use method additions

--- src/minicraft/gfx/Sprite.java
+++ src/minicraft/gfx/Sprite.java
@@ -11,27 +11,40 @@ public class Sprite {
 	*/
 	
 	public Px[][] spritePixels;
+	public int color;
 	/// spritePixels is arranged so that the pixels are in their correct positions relative to the top left of the full sprite. This means that their render positions are built-in to the array.
 	
 	public Sprite(int sx, int sy, int sw, int sh) {
+		this(sx, sy, sw, sh, 0, false, false);}
+	public Sprite(int sx, int sy, int sw, int sh, int color) {
+		this(sx, sy, sw, sh, color, false, false);}
+	public Sprite(int sx, int sy, int sw, int sh, boolean mirrorHori, boolean mirrorVert) {
+		this(sx, sy, sw, sh, 0, mirrorHori, mirrorVert);}
+	public Sprite(int sx, int sy, int sw, int sh, int color, boolean mirrorHori, boolean mirrorVert) {
 		spritePixels = new Px[sh][sw];
+		this.color = color;
+		int mirrorBits = (mirrorHori ? 1 : 0) + (mirrorVert ? 2 : 0);
 		for(int r = 0; r < sh; r++)
 			for(int c = 0; c < sw; c++)
-				spritePixels[r][c] = new Px(sx+c, sy+r, 0);
+				spritePixels[r][c] = new Px(sx+c, sy+r, mirrorBits);
 	}
 	
-	public Sprite(Px[][] pixels) {
+	public Sprite(Px[][] pixels) { this(pixels, 0); }
+	public Sprite(Px[][] pixels, int color) {
 		spritePixels = pixels;
+		this.color = color;
 	}
 	
+	public void render(Screen screen, int lvlx, int lvly) { render(screen, color, lvlx, lvly); }
 	public void render(Screen screen, int color, int lvlx, int lvly) {
 		/// here, x and y are entity coordinates, I think.
 		
-		for(int r = 0; r < spritePixels.length; r++) { // loop down through each row
-			renderRow(r, screen, color, lvlx, lvly + r*8);
+		for(int row = 0; row < spritePixels.length; row++) { // loop down through each row
+			renderRow(row, screen, color, lvlx, lvly + row*8);
 		}
 	}
 	
+	public void renderRow(int r, Screen screen, int x, int y) { renderRow(r, screen, color, x, y); }
 	public void renderRow(int r, Screen screen, int color, int x, int y) {
 		Px[] row = spritePixels[r];
 		for(int c = 0; c < row.length; c++) { // loop across through each column
