32e2cbfc9a124d990ff06e00cf857a96ca845e27
Christopher Johns <cjohns4288@gmail.com>
2017-06-10 20:44:00 -0700

    remodeled RemotePlayer class

--- src/minicraft/entity/RemotePlayer.java
+++ src/minicraft/entity/RemotePlayer.java
@@ -1,11 +1,34 @@
 package minicraft.entity;
 
+import java.net.InetAddress;
 import minicraft.Game;
+import minicraft.InputHandler;
+import minicraft.gfx.Screen;
 
 /** This is used only by the Server runtime, to represent a client player. */
 public class RemotePlayer extends Player {
 	
-	public RemotePlayer(Game game) {
-		super(game, game.input);
+	public InetAddress ipAddress;
+	public int port;
+	
+	public String username;
+	
+	public RemotePlayer(Game game, String username, InetAddress ip, int port) {
+		super(game, new InputHandler(game, false));
+		this.ipAddress = ip;
+		this.port = port;
+		this.username = username;
+	}
+	public RemotePlayer(Game game, RemotePlayer model) {
+		this(game, model.username, model.ipAddress, model.port);
+	}
+	
+	public void render(Screen screen) {
+		super.render(screen);
+		Font.drawCentered(username, screen, y - 10, Color.get(-1, 444)); // draw the username of the player above their head
+	}
+	
+	public String getClientName() {
+		return ipAddress.getHostAddress();
 	}
 }
