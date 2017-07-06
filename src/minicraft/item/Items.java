8979399457a7a79930e132031f376dfa9ab8755f
Christopher Johns <cjohns4288@gmail.com>
2017-07-06 14:45:58 -0700

    removed old code + updated debug statements

--- src/minicraft/item/Items.java
+++ src/minicraft/item/Items.java
@@ -3,17 +3,13 @@ package minicraft.item;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import minicraft.Game;
 import minicraft.entity.Inventory;
-// import java.io.File;
-// import java.net.URL;
-// import java.net.URI;
-// import java.net.URISyntaxException;
 
 public class Items {
 	
 	/// I've checked -- this is only used for making the creative inventory, and in Load.java.
-	/// ...well, that used to be true...  && !name.contains(";)
-	
+	/// ...well, that used to be true...
 	
 	/**
 		Ok, so here's the actual big idea:
@@ -26,7 +22,7 @@ public class Items {
 	
 	private static void add(Item i) {
 		String name = i.name.toUpperCase();
-		//System.out.println("adding " + name);
+		//if(Game.debug) System.out.println("adding " + name);
 		items.add(i);
 	}
 	private static void addAll(ArrayList<Item> items) {
@@ -34,46 +30,6 @@ public class Items {
 	}
 	
 	static {
-		//String path = "/home/chris/Documents/minicraft/minicraft-plus-revived/src/minicraft/item";
-		/*try {
-			//Items.class.getClassLoader().getResource("minicraft/item/");
-			System.out.println("url: " + url);
-			path = url.toURI();
-		} catch(URISyntaxException ex) {
-			ex.printStackTrace();
-		}*/
-		/*System.out.println("path of classes: " + path);
-		File dir = new File(path);
-		System.out.println("path exists: " + dir.exists());
-		List<String> classes = Arrays.asList((new File(path)).list(new java.io.FilenameFilter() {
-			public boolean accept(File file, String name) {
-				return true;//!file.isDirectory() && name.endsWith(".java") && !file.getName().equals("Items.java") && !file.getName().equals("Item.java");
-			}
-		}));
-		System.out.println("classes: " + classes);
-		classes = new ArrayList<String>(classes);
-		for(int i = 0; i < classes.size(); i++) {
-			String clazz = classes.get(i);
-			System.out.println("found class: " + clazz);
-			if(!clazz.contains(".java") || clazz.equals("Item.java") || clazz.equals("Items.java")) {
-				classes.remove(i);
-				i--;
-			}
-			else {
-				classes.set(i, "minicraft.item."+clazz.replace(".java", ""));
-				System.out.println("kept class " + clazz);
-			}
-		}
-		
-		//now i have the class package paths, to pass to Class.forName().
-		try {
-			for(String clazz: classes) {
-				System.out.println(clazz);
-				addAll((ArrayList<Item>)Class.forName(clazz).getMethod("getAllInstances").invoke(null));
-			}
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}*/
 		addAll(PowerGloveItem.getAllInstances());
 		addAll(FurnitureItem.getAllInstances());
 		addAll(TorchItem.getAllInstances());
@@ -91,7 +47,7 @@ public class Items {
 	/** fetches an item from the list given it's name. I mean, I would have just used a HashMap... Hey, look! I'm using one! ^v^ */
 	public static Item get(String name) {
 		name = name.toUpperCase();
-		//System.out.println("fetching name: " + name);
+		//System.out.println("fetching name: \"" + name + "\"");
 		int amount = 1;
 		if(name.contains("_")) {
 			try {
@@ -116,7 +72,8 @@ public class Items {
 				((StackableItem)i).count = amount;
 			return i.clone();
 		} else {
-			System.out.println("ITEMS GET: invalid name requested: " + name);
+			System.out.println(Game.onlinePrefix()+"ITEMS GET: invalid name requested: \"" + name + "\"");
+			//Thread.dumpStack();
 			return get("POWER GLOVE");
 		}
 		/*if(!name.equals("")) { // name is not nothing
@@ -140,12 +97,5 @@ public class Items {
 			inv.add(item.clone());
 		}
 	}
-	/*
-	/// I am not horribly proud of this method... but it had to be done... for the sake of the Recipes...
-	public static Item[] get(String name, int amount) {
-		Item[] items = new Item[amount];
-		Arrays.fill(items, get(name));
-		return items;
-	}*/
 }
 	
