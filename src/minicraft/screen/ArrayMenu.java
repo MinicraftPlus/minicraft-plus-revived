ab690dbded8e94055978549e533cc845699e2a3c
Christopher Johns <cjohns4288@gmail.com>
2017-08-06 21:51:09 -0700

    made template for new ArrayMenu class

---
+++ src/minicraft/screen/ArrayMenu.java
@@ -0,0 +1,11 @@
+package minicraft.screen;
+
+
+/// This class is designed for those menus that are not so much a list of options, where you select one to do an action, but rather a list of variables, each of which you assign a value. Basically, this is for settings menus.
+public class ArrayMenu extends ScrollingMenu {
+	
+	/** Plans:
+		-mainly follow the lead of WorldGenMenu.
+	*/
+	
+}
