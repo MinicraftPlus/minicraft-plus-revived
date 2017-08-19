6646db5b415975eace22e0abe6aa183356df2a81
Christopher Johns <cjohns4288@gmail.com>
2017-08-19 16:46:35 -0700

    added increment field

--- src/minicraft/screen/entry/RangeEntry.java
+++ src/minicraft/screen/entry/RangeEntry.java
@@ -1,30 +1,29 @@
 package minicraft.screen.entry;
 
-import minicraft.InputHandler;
-import minicraft.Sound;
-import minicraft.gfx.FontStyle;
-import minicraft.gfx.Screen;
-
 /**
  * Sort of like a slider; it denotes a range of number values.
  */
 public class RangeEntry extends ArrayEntry<Integer> {
 	
-	private int minVal;
+	private int minVal, increment;
 	
 	public RangeEntry(String label, int minVal, int maxVal, int startVal) {
-		super(label, maxVal-minVal, startVal);
+		this(label, minVal, maxVal, startVal, 1);
+	}
+	public RangeEntry(String label, int minVal, int maxVal, int startVal, int inc) {
+		super(label, maxVal-minVal - ((maxVal-minVal)%inc), startVal);
 		this.minVal = minVal;
+		this.increment = inc;
 	}
 	
 	@Override
 	public Integer getValue() {
-		return getIndex() + minVal;
+		return increment * getIndex() + minVal;
 	}
 	
 	@Override
 	public void setValue(Integer value) {
-		setIndex(clamp(value, minVal, minVal + getNumChoices() - 1));
+		setIndex(clamp(value - ((value-minVal) % increment), minVal, minVal + increment * (getNumChoices() - 1)));
 	}
 	
 	public static int clamp(int val, int min, int max) {
