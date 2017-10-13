e1a64e94b21281a65158c057c9bd902daa2b8374
Christopher Johns <cjohns4288@gmail.com>
2017-10-13 10:19:05 -0700

    fixed book displays

--- src/minicraft/screen/BookDisplay.java
+++ src/minicraft/screen/BookDisplay.java
@@ -1,14 +1,16 @@
 package minicraft.screen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import minicraft.Game;
 import minicraft.InputHandler;
 import minicraft.gfx.Color;
 import minicraft.gfx.Font;
-import minicraft.gfx.FontStyle;
-import minicraft.gfx.Rectangle;
+import minicraft.gfx.Point;
 import minicraft.gfx.Screen;
+import minicraft.gfx.SpriteSheet;
+import minicraft.screen.entry.StringEntry;
 
 public class BookDisplay extends Display {
 	
@@ -18,7 +20,7 @@ public class BookDisplay extends Display {
 	private static final int spacing = 3;
 	//private static java.awt.Rectangle textArea = new java.awt.Rectangle(15, 8*5, 8*32, 8*16);
 	private static final int minX = 15, maxX = 15+8*32, minY = 8*5, maxY = 8*5+8*16;
-	private static final FontStyle style = new FontStyle(Color.BLACK).xCenterBounds(minX, maxX).yCenterBounds(minY, maxY);
+	//private static final FontStyle style = new FontStyle(Color.BLACK).xCenterBounds(minX, maxX).yCenterBounds(minY, maxY);
 	
 	private String[][] lines;
 	private int page;
@@ -49,48 +51,59 @@ public class BookDisplay extends Display {
 			String[] remainder = {content};
 			while(remainder[remainder.length-1].length() > 0) {
 				remainder = Font.getLines(remainder[remainder.length-1], maxX-minX, maxY-minY, spacing);
-				pages.add(remainder);//Arrays.copyOf(remainder, remainder.length-1)); // removes the last element of remainder, which is the leftover.
+				pages.add(Arrays.copyOf(remainder, remainder.length-1)); // removes the last element of remainder, which is the leftover.
 			}
 		}
 		
 		lines = pages.toArray(new String[pages.size()][]);
 		
-		Menu.Builder builder = new Menu.Builder(true, 0, RelPos.CENTER)
-			.setFrame(Color.DARK_GRAY, Color.get(554, 554), Color.get(-1, 1, 554, 554));
+		Menu.Builder builder = new Menu.Builder(true, spacing, RelPos.CENTER)
+			.setFrame(554, 1, 554);
 		
-		menus = new Menu[] {
-			builder
-				.setBounds(new Rectangle(14*8, 0, 21*8, 3*8, Rectangle.CORNERS))
-				.createMenu(),
-			
-			builder
-				.setBounds(new Rectangle(1*8, 4*8, 34*8, 20*8, Rectangle.CORNERS))
-				.createMenu()
-		};
+		Menu pageCount = builder // the small rect for the title
+			//.setBounds(new Rectangle(14*8, 0, 21*8, 3*8, Rectangle.CORNERS))
+			.setPositioning(new Point(Screen.w/2, 0), RelPos.BOTTOM)
+			.setEntries(StringEntry.useLines(Color.BLACK, "Page", "Title"))
+			.setSelection(1)
+			.createMenu();
+		
+		// now create a menu for each page TODO this could be optimized to only update the entries, perhaps...
+		
+		builder
+			//.setBounds(new Rectangle(1*8, 4*8, 34*8, 20*8, Rectangle.CORNERS))
+			.setPositioning(new Point(Screen.w/2, pageCount.getBounds().getBottom() + spacing), RelPos.BOTTOM)
+			.setSize(maxX-minX + SpriteSheet.boxWidth*2, maxY-minY + SpriteSheet.boxWidth*2)
+			.setShouldRender(false);
+		
+		menus = new Menu[lines.length+1];
+		menus[0] = pageCount;
+		for(int i = 0; i < lines.length; i++) {
+			menus[i+1] = builder.setEntries(StringEntry.useLines(Color.BLACK, lines[i])).createMenu();
+		}
+		
+		menus[page+1].shouldRender = true;
+	}
+	
+	private void turnPage(int dir) {
+		if(page+dir >= 0 && page+dir < lines.length) {
+			menus[page+1].shouldRender = false;
+			page += dir;
+			menus[0].updateSelectedEntry(new StringEntry(page==0?"Title":page+"", Color.BLACK));
+			menus[page+1].shouldRender = true;
+		}
 	}
 	
 	@Override
 	public void tick(InputHandler input) {
 		if (input.getKey("menu").clicked || input.getKey("exit").clicked)
 			Game.exitMenu(); // this is what closes the book; TODO if books were editable, I would probably remake the book here with the edited pages.
-		if (input.getKey("left").clicked && page > 0) page--; // this is what turns the page back
-		if (input.getKey("right").clicked && page < lines.length-1) page++; // this is what turns the page forward
+		if (input.getKey("left").clicked) turnPage(-1); // this is what turns the page back
+		if (input.getKey("right").clicked) turnPage(1); // this is what turns the page forward
 	}
 	
-	@Override
+	/*@Override
 	public void render(Screen screen) {
 		super.render(screen);
-		//renderFrames(screen);
-		//new Frame("", new Rectangle(14, 0, 21, 3, Rectangle.CORNERS)).setColors(Color.DARK_GRAY, Color.get(554, 554), Color.get(-1, 1, 554, 554)).render(screen);
-		//new Frame("", new Rectangle(1, 4, 34, 20, Rectangle.CORNERS)).setColors(Color.DARK_GRAY, Color.get(554, 554), Color.get(-1, 1, 554, 554)).render(screen);
-		
-		// This draws the text "Page" at the top of the screen
-		//System.out.println("drawing page num text");
-		Font.draw("Page", screen, Font.textHeight() * 16, Font.textHeight() - 2, Color.BLACK);
-		
-		// This is makes the numbers appear below "Page" // ...but it doesn't work...
-		String pagenum = page==0?"Title": page+"";
-		Font.drawCentered(pagenum, screen, /*11*11 + (page==0 ? 4 : 21-3*digits(page)), */2 * 8, Color.BLACK);
 		
 		if(page != 0) style.setXPos(minX); // center text on the title page
 		else style.setXPos(-1); // don't center after title page
@@ -98,5 +111,5 @@ public class BookDisplay extends Display {
 		style.yCenterBounds(minY, maxY);
 		
 		Font.drawParagraph(lines[page], screen, style, spacing); // draw the current page
-	}
+	}*/
 }
--- src/minicraft/screen/Displays.java
+++ src/minicraft/screen/Displays.java
@@ -6,8 +6,8 @@ import minicraft.saveload.Load;
 
 public class Displays {
 	
-	public static final String about = "This is the about page.";
-	public static final String instructions = "This is the instructions page.";
+	public static final String about = "This is the about page.\0There is not much to see here.";
+	public static final String instructions = "This is the instructions page.\0I hope to put more\nhere\nlater.";
 	
 	public static final String antVenomBook;
 	static {
