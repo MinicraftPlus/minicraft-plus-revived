4b6e4805e3fed0cf2faa30b5003ae52b307a8c99
Christopher Johns <cjohns4288@gmail.com>
2017-10-16 21:17:23 -0700

    made DeadMenu look better

--- src/minicraft/screen/DeadMenu.java
+++ src/minicraft/screen/DeadMenu.java
@@ -1,18 +1,19 @@
 package minicraft.screen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import minicraft.Game;
 import minicraft.Settings;
 import minicraft.gfx.Point;
 import minicraft.gfx.SpriteSheet;
+import minicraft.screen.entry.BlankEntry;
 import minicraft.screen.entry.ListEntry;
 import minicraft.screen.entry.SelectEntry;
 import minicraft.screen.entry.StringEntry;
 
 public class DeadMenu extends Display {
-	// TODO implement an optional input delay variable for Displays
-	private int inputDelay = 60;
+	//private int inputDelay = 60;
 	// this is an IMPORTANT bool, determines if the user should respawn or not. :)
 	public static boolean shouldRespawn;
 	
@@ -20,9 +21,13 @@ public class DeadMenu extends Display {
 		super(false, false);
 		
 		ArrayList<ListEntry> entries = new ArrayList<>();
-		entries.add(new StringEntry("Time: " + InfoDisplay.getTimeString()));
-		entries.add(new StringEntry("Score: " + Game.player.score));
-		entries.add(new SelectEntry("Quit", () -> Game.setMenu(new TitleMenu())));
+		entries.addAll(Arrays.asList(
+			new StringEntry("Time: " + InfoDisplay.getTimeString()),
+			new StringEntry("Score: " + Game.player.score),
+			new BlankEntry(),
+			new SelectEntry("Quit", () -> Game.setMenu(new TitleMenu()))
+		));
+		
 		if(!Settings.get("mode").equals("hardcore")) {
 			entries.add(new SelectEntry("Respawn", () -> {
 				Game.resetGame();
