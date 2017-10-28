b1dd4930b88a1fc31714762ecad425674d961f1c
ChristofferHolmesland <deadblade97@gmail.com>
2017-10-28 23:28:34 +0200

    WIP: Javadoc comments for the Entry package

--- src/minicraft/screen/entry/SelectEntry.java
+++ src/minicraft/screen/entry/SelectEntry.java
@@ -9,11 +9,21 @@ public class SelectEntry extends ListEntry {
 	private Action onSelect;
 	private String text;
 	
+	/**
+	 * Creates a new entry which acts as a button. 
+	 * Can do an action when it is selected.
+	 * @param text Text displayed on this entry
+	 * @param onSelect Action which happens when the entry is selected
+	 */
 	public SelectEntry(String text, Action onSelect) {
 		this.onSelect = onSelect;
 		this.text = text;
 	}
 	
+	/**
+	 * Changes the text of the entry.
+	 * @param text new text
+	 */
 	void setText(String text) { this.text = text; }
 	
 	@Override
@@ -29,6 +39,7 @@ public class SelectEntry extends ListEntry {
 		return Font.textWidth(text);
 	}
 	
+	@Override
 	public String toString() {
 		return text;
 	}
