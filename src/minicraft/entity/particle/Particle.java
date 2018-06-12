183a5070060fa22b0a2d0c18612e16a79fc84f3d
Christopher Johns <cjohns4288@gmail.com>
2018-06-12 16:25:39 -0700

    modified to merge with upstream commits

--- src/minicraft/entity/particle/Particle.java
+++ src/minicraft/entity/particle/Particle.java
@@ -11,6 +11,14 @@ public class Particle extends Entity implements ClientTickable {
 	
 	protected Sprite sprite;
 	
+	/**
+	 * Creates an particle entity at the given position. The particle has a x and y radius = 1.
+	 * @param x X map coordinate
+	 * @param y Y map coorindate
+	 * @param xr x radius of the particle   
+	 * @param lifetime How many game ticks the particle lives before its removed
+	 * @param sprite The particle's sprite
+	 */
 	public Particle(int x, int y, int xr, int lifetime, Sprite sprite) {
 		// make a particle at the given coordinates
 		super(xr, 1);
@@ -23,14 +31,16 @@ public class Particle extends Entity implements ClientTickable {
 	public Particle(int x, int y, int lifetime, Sprite sprite) {
 		this(x, y, 1, lifetime, sprite);
 	}
-
+	
+	@Override
 	public void tick() {
 		time++;
 		if(time > lifetime) {
 			remove();
 		}
 	}
-
+	
+	@Override
 	public void render(Screen screen) { sprite.render(screen, x, y); }
 	
 	@Override
