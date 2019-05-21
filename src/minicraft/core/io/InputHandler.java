1f1adfeb77e0fdaa06156ed8b042b15eec9c8f95
africalinguist <26539285+africalinguist@users.noreply.github.com>
2019-05-21 07:17:03 +0630

    Remove them entirely

--- src/minicraft/core/io/InputHandler.java
+++ src/minicraft/core/io/InputHandler.java
@@ -98,10 +98,6 @@ public class InputHandler implements /*MouseListener, */KeyListener {
 	}
 	
 	private void initKeyMap() {
-		keymap.put("UP", "UP"); //up action references up arrow key
-		keymap.put("DOWN", "DOWN"); //move down action references down arrow key
-		keymap.put("LEFT", "LEFT"); //move left action references left arrow key
-		keymap.put("RIGHT", "RIGHT"); //move right action references right arrow key
 		
 		keymap.put("SELECT", "ENTER");
 		keymap.put("EXIT", "ESCAPE");
