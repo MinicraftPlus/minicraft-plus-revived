8379368eab3aa96bdd50688e32a76ed90e4dc416
afyber <26539285+africalinguist@users.noreply.github.com>
2019-10-06 16:36:45 +0630

    Fixed the rest of the stackable items

--- src/minicraft/item/StackableItem.java
+++ src/minicraft/item/StackableItem.java
@@ -19,21 +19,21 @@ public class StackableItem extends Item {
 		items.add(new StackableItem("Wheat", new Sprite(6, 0, 0)));
 		items.add(new StackableItem("Key", new Sprite(0, 4, 0)));
 		items.add(new StackableItem("arrow", new Sprite(0, 2, 0)));
-		items.add(new StackableItem("string", new Sprite(25, 4, 0)));
-		items.add(new StackableItem("Coal", new Sprite(10, 4, 0)));
-		items.add(new StackableItem("Iron Ore", new Sprite(10, 4, 0)));
-		items.add(new StackableItem("Lapis", new Sprite(10, 4, 0)));
-		items.add(new StackableItem("Gold Ore", new Sprite(10, 4, 0)));
-		items.add(new StackableItem("Iron", new Sprite(11, 4, 0)));
-		items.add(new StackableItem("Gold", new Sprite(11, 4, 0)));
+		items.add(new StackableItem("string", new Sprite(1, 4, 0)));
+		items.add(new StackableItem("Coal", new Sprite(2, 4, 0)));
+		items.add(new StackableItem("Iron Ore", new Sprite(3, 4, 0)));
+		items.add(new StackableItem("Lapis", new Sprite(4, 4, 0)));
+		items.add(new StackableItem("Gold Ore", new Sprite(5, 4, 0)));
+		items.add(new StackableItem("Iron", new Sprite(6, 4, 0)));
+		items.add(new StackableItem("Gold", new Sprite(7, 4, 0)));
 		items.add(new StackableItem("Rose", new Sprite(5, 0, 0)));
-		items.add(new StackableItem("GunPowder", new Sprite(22, 4, 0)));
-		items.add(new StackableItem("Slime", new Sprite(10, 4, 0)));
-		items.add(new StackableItem("glass", new Sprite(12, 4, 0)));
-		items.add(new StackableItem("cloth", new Sprite(9, 0, 0)));
-		items.add(new StackableItem("gem", new Sprite(13, 4, 0)));
-		items.add(new StackableItem("Scale", new Sprite(22, 4, 0)));
-		items.add(new StackableItem("Shard", new Sprite(23, 4, 0)));
+		items.add(new StackableItem("GunPowder", new Sprite(8, 4, 0)));
+		items.add(new StackableItem("Slime", new Sprite(9, 4, 0)));
+		items.add(new StackableItem("glass", new Sprite(10, 4, 0)));
+		items.add(new StackableItem("cloth", new Sprite(11, 4, 0)));
+		items.add(new StackableItem("gem", new Sprite(12, 4, 0)));
+		items.add(new StackableItem("Scale", new Sprite(13, 4, 0)));
+		items.add(new StackableItem("Shard", new Sprite(14, 4, 0)));
 		
 		return items;
 	}
