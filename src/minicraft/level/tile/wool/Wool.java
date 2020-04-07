74f4b5624fabef1f748eb927f157c43e8a5fd06d
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2020-04-07 17:49:48 +0200

    Fixed indent

--- src/minicraft/level/tile/wool/Wool.java
+++ src/minicraft/level/tile/wool/Wool.java
@@ -16,27 +16,27 @@ import minicraft.level.tile.Tiles;
 public class Wool extends Tile {
 	private String woolType;
 
-    public Wool(String name, Sprite sprite, String woolType) {
-        super(name, sprite);
-        this.woolType = woolType;
-    }
+	public Wool(String name, Sprite sprite, String woolType) {
+		super(name, sprite);
+		this.woolType = woolType;
+	}
 
-    public boolean interact(Level level, int xt, int yt, Player player, Item item, Direction attackDir) {
-        if (item instanceof ToolItem) {
-            ToolItem tool = (ToolItem) item;
-            if (tool.type == ToolType.Shovel) {
-                if (player.payStamina(3 - tool.level) && tool.payDurability()) {
-                    level.setTile(xt, yt, Tiles.get("hole"));
-                    Sound.monsterHurt.play();
-                    level.dropItem(xt*16+8, yt*16+8, Items.get(woolType));
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	public boolean interact(Level level, int xt, int yt, Player player, Item item, Direction attackDir) {
+		if (item instanceof ToolItem) {
+			ToolItem tool = (ToolItem) item;
+			if (tool.type == ToolType.Shovel) {
+				if (player.payStamina(3 - tool.level) && tool.payDurability()) {
+					level.setTile(xt, yt, Tiles.get("hole"));
+					Sound.monsterHurt.play();
+					level.dropItem(xt*16+8, yt*16+8, Items.get(woolType));
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    public boolean mayPass(Level level, int x, int y, Entity e) {
-        return e.canWool();
-    }
+	public boolean mayPass(Level level, int x, int y, Entity e) {
+		return e.canWool();
+	}
 }
