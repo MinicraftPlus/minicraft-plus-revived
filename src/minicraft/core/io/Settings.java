019b455a20ff812cdde847b7caff64a450e44ac9
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2020-07-17 20:21:02 +0200

    Settings: Initial FPS is set to screen refresh rate if there is a screen.

--- src/minicraft/core/io/Settings.java
+++ src/minicraft/core/io/Settings.java
@@ -1,18 +1,27 @@
 package minicraft.core.io;
 
-import java.awt.GraphicsEnvironment;
-import java.util.HashMap;
-
 import minicraft.screen.entry.ArrayEntry;
 import minicraft.screen.entry.BooleanEntry;
 import minicraft.screen.entry.RangeEntry;
+
+import java.awt.*;
+import java.util.HashMap;
 
 public class Settings {
 	
 	private static HashMap<String, ArrayEntry> options = new HashMap<>();
 	
 	static {
-		options.put("fps", new RangeEntry("Max FPS", 10, 300, 60));
+
+		int initialFPS = 60;
+		if (!GraphicsEnvironment.isHeadless()) {
+			int screenRefreshRate = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate();
+
+			if (screenRefreshRate != DisplayMode.REFRESH_RATE_UNKNOWN)
+				initialFPS = screenRefreshRate;
+		}
+
+		options.put("fps", new RangeEntry("Max FPS", 10, 300, initialFPS));
 		options.put("diff", new ArrayEntry<>("Difficulty", "Easy", "Normal", "Hard"));
 		options.get("diff").setSelection(1);
 		options.put("mode", new ArrayEntry<>("Game Mode", "Survival", "Creative", "Hardcore", "Score"));
