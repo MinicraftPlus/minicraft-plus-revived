c435566a84ffc84f18affad325da608d9389ed97
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2020-07-18 20:27:28 +0200

    Boat: The player can now pick up the boat

--- src/minicraft/item/PowerGloveItem.java
+++ src/minicraft/item/PowerGloveItem.java
@@ -1,5 +1,6 @@
 package minicraft.item;
 
+import minicraft.entity.Boat;
 import minicraft.entity.Direction;
 import minicraft.entity.Entity;
 import minicraft.entity.furniture.Furniture;
@@ -16,6 +17,10 @@ public class PowerGloveItem extends Item {
 		if (entity instanceof Furniture) { // If the power glove is used on a piece of furniture...
 			Furniture f = (Furniture) entity;
 			f.take(player); // Takes (picks up) the furniture
+			return true;
+		} else if (entity instanceof Boat) {
+			Boat b = (Boat)entity;
+			b.take(player);
 			return true;
 		}
 		return false; // method returns false if we were not given a furniture entity.
