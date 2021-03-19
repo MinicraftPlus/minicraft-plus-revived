4f294adfdee008bb38c7012d3a62ae838287c240
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2021-03-19 11:49:59 +0100

    Wool: Require shear to remove

--- src/minicraft/level/tile/Wool.java
+++ src/minicraft/level/tile/Wool.java
@@ -1,4 +1,4 @@
-package minicraft.level.tile.wool;
+package minicraft.level.tile;
 
 import minicraft.core.io.Sound;
 import minicraft.entity.Direction;
@@ -10,25 +10,21 @@ import minicraft.item.Items;
 import minicraft.item.ToolItem;
 import minicraft.item.ToolType;
 import minicraft.level.Level;
-import minicraft.level.tile.Tile;
-import minicraft.level.tile.Tiles;
 
 public class Wool extends Tile {
-	private String woolType;
 
-	public Wool(String name, Sprite sprite, String woolType) {
-		super(name, sprite);
-		this.woolType = woolType;
+	public Wool(String name, WoolType woolType) {
+		super(name, woolType.sprite);
 	}
 
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, Direction attackDir) {
 		if (item instanceof ToolItem) {
 			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.Shovel) {
+			if (tool.type == ToolType.Shear) {
 				if (player.payStamina(3 - tool.level) && tool.payDurability()) {
 					level.setTile(xt, yt, Tiles.get("hole"));
 					Sound.monsterHurt.play();
-					level.dropItem(xt*16+8, yt*16+8, Items.get(woolType));
+					level.dropItem(xt * 16 + 8, yt * 16 + 8, Items.get(name));
 					return true;
 				}
 			}
@@ -38,5 +34,25 @@ public class Wool extends Tile {
 
 	public boolean mayPass(Level level, int x, int y, Entity e) {
 		return e.canWool();
+	}
+
+	public enum WoolType {
+		BLACK(new Sprite(10, 4, 2, 2, 1)),
+		BLUE(new Sprite(8, 2, 2, 2, 1)),
+		GREEN(new Sprite(10, 2, 2, 2, 1)),
+		NORMAL(new Sprite(8, 0, 2, 2, 1)),
+		RED(new Sprite(10, 0, 2, 2, 1)),
+		YELLOW(new Sprite(8, 4, 2, 2, 1));
+
+		public final Sprite sprite;
+
+		/**
+		 * Create a type of wool.
+		 *
+		 * @param sprite The sprite for the type of wool.
+		 */
+		WoolType(Sprite sprite) {
+			this.sprite = sprite;
+		}
 	}
 }
