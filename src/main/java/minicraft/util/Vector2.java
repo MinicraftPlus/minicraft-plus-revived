e906311c014bb0695c1e038f2a9445fb7f910de3
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2021-09-07 13:58:38 +0200

    Util: Created class for vectors (transform)

---
+++ src/main/java/minicraft/util/Vector2.java
@@ -0,0 +1,24 @@
+package minicraft.util;
+
+public class Vector2 {
+	public double x, y;
+
+	public Vector2(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vector2 normalized() {
+		double max = Math.max(x, y);
+		return new Vector2(x/max, y/max);
+	}
+
+	public static Vector2 normalize(Vector2 vec) {
+		double max = Math.max(vec.x, vec.y);
+		return new Vector2(vec.x/max, vec.y/max);
+	}
+
+	public static double distance(double x, double y) {
+		return Math.sqrt(x*x + y*y);
+	}
+}
