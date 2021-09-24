e95701973c9060bc5600f40bf8205d9f17de9b34
Litorom <70768734+Litorom@users.noreply.github.com>
2021-09-24 19:35:41 -0400

    Bug Fixed: TexturePackDisplay.java menu wouldn't update

--- src/main/java/minicraft/screen/TexturePackDisplay.java
+++ src/main/java/minicraft/screen/TexturePackDisplay.java
@@ -77,7 +77,7 @@ public class TexturePackDisplay extends Display {
 
 	@Override
 	public void tick(InputHandler input) {
-		if (input.getKey("menu").clicked || input.getKey("attack").clicked || input.getKey("exit").clicked) {
+		if (input.getKey("exit").clicked) {
 			Game.exitMenu();
 			return;
 		}
@@ -91,6 +91,7 @@ public class TexturePackDisplay extends Display {
 		}
         if (input.getKey("SELECT").clicked) {
             shouldUpdate = true;
+            Sound.confirm.play();
         }
 	}
 
