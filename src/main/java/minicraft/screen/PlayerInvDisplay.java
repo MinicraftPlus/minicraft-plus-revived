077da173b58c18f30f4810de52f264150ccf08b4
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2021-12-07 12:42:01 +0100

    Inventory: Moved inventory searcher help text
    This should make it more comfortable to read.

--- src/main/java/minicraft/screen/PlayerInvDisplay.java
+++ src/main/java/minicraft/screen/PlayerInvDisplay.java
@@ -37,7 +37,8 @@ public class PlayerInvDisplay extends Display {
 	public void render(Screen screen) {
 		super.render(screen);
 
+		// Searcher help text
 		String text = "(" + Game.input.getMapping("SEARCHER-BAR") + ") " + Localization.getLocalized("to search.");
-		Font.draw(text, screen, Screen.w / 2 - text.length(), Screen.h / 2 + 16, Color.WHITE);
+		Font.draw(text, screen, 16, Screen.h / 2 + 2, Color.WHITE);
 	}
 }
