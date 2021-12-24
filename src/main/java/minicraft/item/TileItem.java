ac026e00c892544119ffddc3c30b0c2b06624ac0
Litorom <70768734+Litorom@users.noreply.github.com>
2021-12-24 11:46:15 -0500

    Corrected bug

--- src/main/java/minicraft/item/TileItem.java
+++ src/main/java/minicraft/item/TileItem.java
@@ -31,13 +31,13 @@ public class TileItem extends StackableItem {
 		items.add(new TileItem("Stone", (new Sprite(2, 0, 0)), "Stone", "hole", "water", "cloud", "lava"));
 		items.add(new TileItem("Stone Brick", (new Sprite(3, 5, 0)), "Stone Bricks", "hole", "water", "cloud", "lava"));
 		items.add(new TileItem("Ornate Stone", (new Sprite(3, 5, 0)), "Ornate Stone", "hole", "water", "cloud", "lava"));
-		items.add(new TileItem("Stone Wall", (new Sprite(4, 5, 0)), "Stone Wall", "Stone Bricks", "Raw Stone", "Ornate Stone"));
-		items.add(new TileItem("Stone Door", (new Sprite(5, 5, 0)), "Stone Door", "Stone Bricks", "Raw Stone", "Ornate Stone"));
+		items.add(new TileItem("Stone Wall", (new Sprite(4, 5, 0)), "Stone Wall", "Stone Bricks"));
+		items.add(new TileItem("Stone Door", (new Sprite(5, 5, 0)), "Stone Door", "Stone Bricks"));
 		items.add(new TileItem("Raw Obsidian", (new Sprite(9, 5, 0)), "Raw Obsidian", "hole", "water", "cloud", "lava"));
 		items.add(new TileItem("Obsidian Brick", (new Sprite(6, 5, 0)), "Obsidian", "hole", "water", "cloud", "lava"));
 		items.add(new TileItem("Obsidian Tiles", (new Sprite(6, 5, 0)), "Obsidian Tiles","hole", "water", "cloud", "lava"));
-		items.add(new TileItem("Obsidian Wall", (new Sprite(7, 5, 0)), "Obsidian Wall", "Obsidian", "Raw Obsidian", "Obsidian Tiles"));
-		items.add(new TileItem("Obsidian Door", (new Sprite(8, 5, 0)), "Obsidian Door", "Obsidian", "Raw Obsidian", "Obsidian Tiles"));
+		items.add(new TileItem("Obsidian Wall", (new Sprite(7, 5, 0)), "Obsidian Wall", "Obsidian"));
+		items.add(new TileItem("Obsidian Door", (new Sprite(8, 5, 0)), "Obsidian Door", "Obsidian"));
 	
 		items.add(new TileItem("Wool", (new Sprite(5, 3, 0)), "Wool", "hole", "water"));
 		items.add(new TileItem("Red Wool", (new Sprite(4, 3, 0)), "Red Wool", "hole", "water"));
