85ad78cc10545b1e08f8b522d77eced965c4c6fc
TheBigEye <thebigeyedev@gmail.com>
2022-01-10 20:37:25 -0300

    Load and set the game difficulty

--- src/main/java/minicraft/saveload/Load.java
+++ src/main/java/minicraft/saveload/Load.java
@@ -344,6 +344,7 @@ public class Load {
 		// Settings
 		Settings.set("sound", json.getBoolean("sound"));
 		Settings.set("autosave", json.getBoolean("autosave"));
+		Settings.set("diff", json.getString("diff"));
 		Settings.set("fps", json.getInt("fps"));
 
 		String lang = json.getString("lang");
