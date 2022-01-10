68e0a2870da96b8546eb45c956e37f87b5e149f9
TheBigEye <thebigeyedev@gmail.com>
2022-01-10 17:42:41 -0300

    Fix a small typing error in LevelGen
    Someone put an "n" instead of a "w" being "nval" instead of "wval", this certainly causes a small defect in the generation of the caves.

--- src/main/java/minicraft/level/LevelGen.java
+++ src/main/java/minicraft/level/LevelGen.java
@@ -560,7 +560,7 @@ public class LevelGen {
 				nval = Math.abs(nval - nnoise3.values[i]) * 3 - 2;
 				
 				double wval = Math.abs(wnoise1.values[i] - wnoise2.values[i]);
-				wval = Math.abs(nval - wnoise3.values[i]) * 3 - 2;
+				wval = Math.abs(wval - wnoise3.values[i]) * 3 - 2;
 				
 				double xd = x / (w - 1.0) * 2 - 1;
 				double yd = y / (h - 1.0) * 2 - 1;
