6fba399715f4cc2d85d3f9db00a97ccc6424827b
BenChueng0422 <74168521+BenCheung0422@users.noreply.github.com>
2022-05-06 21:40:57 +0800

    Resolve Spawner sprite problem

--- src/main/java/minicraft/item/FurnitureItem.java
+++ src/main/java/minicraft/item/FurnitureItem.java
@@ -17,6 +17,7 @@ import minicraft.entity.mob.AirWizard;
 import minicraft.entity.mob.Cow;
 import minicraft.entity.mob.Creeper;
 import minicraft.entity.mob.Knight;
+import minicraft.entity.mob.MobAi;
 import minicraft.entity.mob.Pig;
 import minicraft.entity.mob.Player;
 import minicraft.entity.mob.Sheep;
@@ -29,10 +30,10 @@ import minicraft.level.Level;
 import minicraft.level.tile.Tile;
 
 public class FurnitureItem extends Item {
-	
+
 	protected static ArrayList<Item> getAllInstances() {
 		ArrayList<Item> items = new ArrayList<>();
-		
+
 		/// There should be a spawner for each level of mob, or at least make the level able to be changed.
 		items.add(new FurnitureItem(new Spawner(new Cow()), 1, 28));
 		items.add(new FurnitureItem(new Spawner(new Pig()), 2, 28));
@@ -44,10 +45,10 @@ public class FurnitureItem extends Item {
 		items.add(new FurnitureItem(new Spawner(new Snake(1)), 8, 28));
 		items.add(new FurnitureItem(new Spawner(new Knight(1)), 9, 28));
 		items.add(new FurnitureItem(new Spawner(new AirWizard(false)), 10, 28));
-		
+
 		items.add(new FurnitureItem(new Chest()));
 		items.add(new FurnitureItem(new DungeonChest(false, true)));
-		
+
 		// Add the various types of crafting furniture
 		for (Crafter.Type type: Crafter.Type.values()) {
 			 items.add(new FurnitureItem(new Crafter(type)));
@@ -56,29 +57,48 @@ public class FurnitureItem extends Item {
 		for (Lantern.Type type: Lantern.Type.values()) {
 			 items.add(new FurnitureItem(new Lantern(type)));
 		}
-		
+
 		items.add(new FurnitureItem(new Tnt()));
 		items.add(new FurnitureItem(new Bed()));
-		
+
 		return items;
 	}
-	
+
 	public Furniture furniture; // The furniture of this item
 	public boolean placed; // Value if the furniture has been placed or not.
 	private int sx, sy; // Sprite position
-	
+
 	private static int getSpritePos(int fpos) {
 		int x = fpos%32;
 		int y = fpos/32;
 		return ((x-8)/2) + y * 32;
 	}
-	
+
+	private static Sprite getFurnitureSprite(Furniture furniture) {
+		Sprite sprite;
+		if (furniture instanceof Spawner) {
+			MobAi mob = ((Spawner)furniture).mob;
+			if (mob instanceof Cow) sprite = new Sprite(1, 28, 1, 1, 0);
+			else if (mob instanceof Pig) sprite = new Sprite(2, 28, 1, 1, 0);
+			else if (mob instanceof Sheep) sprite = new Sprite(3, 28, 1, 1, 0);
+			else if (mob instanceof Slime) sprite = new Sprite(4, 28, 1, 1, 0);
+			else if (mob instanceof Zombie) sprite = new Sprite(5, 28, 1, 1, 0);
+			else if (mob instanceof Creeper) sprite = new Sprite(6, 28, 1, 1, 0);
+			else if (mob instanceof Skeleton) sprite = new Sprite(7, 28, 1, 1, 0);
+			else if (mob instanceof Snake) sprite = new Sprite(8, 28, 1, 1, 0);
+			else if (mob instanceof Knight) sprite = new Sprite(9, 28, 1, 1, 0);
+			else if (mob instanceof AirWizard) sprite = new Sprite(10, 28, 1, 1, 0);
+			else sprite = new Sprite(getSpritePos(furniture.sprite.getPos()), 0);
+		} else sprite = new Sprite(getSpritePos(furniture.sprite.getPos()), 0);
+		return sprite;
+	}
+
 	public FurnitureItem(Furniture furniture) {
-		super(furniture.name, new Sprite(getSpritePos(furniture.sprite.getPos()), 0));
+		super(furniture.name, getFurnitureSprite(furniture));
 		this.furniture = furniture; // Assigns the furniture to the item
 		placed = false;
 	}
-	
+
 	public FurnitureItem(Furniture furniture, int sx , int sy) {
 		super(furniture.name, new Sprite(sx, sy, 1, 1, 0)); // get the sprite directly
 		this.sx = sx;
@@ -86,12 +106,12 @@ public class FurnitureItem extends Item {
 		this.furniture = furniture;
 		placed = false;
 	}
-	
+
 	/** Determines if you can attack enemies with furniture (you can't) */
 	public boolean canAttack() {
 		return false;
 	}
-	
+
 	/** What happens when you press the "Attack" key with the furniture in your hands */
 	public boolean interactOn(Tile tile, Level level, int xt, int yt, Player player, Direction attackDir) {
 		if (tile.mayPass(level, xt, yt, furniture)) { // If the furniture can go on the tile
@@ -100,22 +120,22 @@ public class FurnitureItem extends Item {
 			// Placed furniture's X and Y positions
 			furniture.x = xt * 16 + 8;
 			furniture.y = yt * 16 + 8;
-			
+
 			level.add(furniture); // Adds the furniture to the world
 			if (Game.isMode("creative"))
 				furniture = furniture.clone();
 			else
 				placed = true; // The value becomes true, which removes it from the player's active item
-			
+
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean isDepleted() {
 		return placed;
 	}
-	
+
 	public FurnitureItem clone() {
 		// in case the item is a spawner, it will use the sprite position (sx, sy)
 		// instead if it is not, the constructor will obtain said sprite
