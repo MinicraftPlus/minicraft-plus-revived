fb85543c420cbbc7e2f7b7d33f79af9b40a19739
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2022-06-05 19:10:46 +0200

    Build: Incremented version to 2.1.2

--- src/main/java/minicraft/core/Game.java
+++ src/main/java/minicraft/core/Game.java
@@ -31,7 +31,7 @@ public class Game {
 	public static boolean debug = false;
 
 	public static final String NAME = "Minicraft Plus"; // This is the name on the application window.
-	public static final Version VERSION = new Version("2.1.1");
+	public static final Version VERSION = new Version("2.1.2");
 
 	public static InputHandler input; // Input used in Game, Player, and just about all the *Menu classes.
 	public static Player player;
@@ -99,7 +99,7 @@ public class Game {
 				} catch (Exception ignored) {
 				}
 			}
-			
+
 			quit();
 		});
 
