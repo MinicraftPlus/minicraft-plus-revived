a9a88918be0a50fea98a74152e3914df9270e771
Litorom <70768734+Litorom@users.noreply.github.com>
2022-08-01 10:43:30 -0400

    Added buttonmaps

--- src/main/java/minicraft/core/io/ControllerHandler.java
+++ src/main/java/minicraft/core/io/ControllerHandler.java
@@ -4,6 +4,8 @@ import com.studiohartman.jamepad.*;
 import minicraft.util.Logging;
 import org.tinylog.Logger;
 
+import java.util.HashMap;
+
 public class ControllerHandler extends ControllerManager {
 	private ControllerManager controllerManager = new ControllerManager();
 	private ControllerIndex controllerIndex;
@@ -18,6 +20,30 @@ public class ControllerHandler extends ControllerManager {
 			Logging.CONTROLLER.debug("No Controllers Detected, moving on.");
 		}
 
+		initButtonMap();
+	}
+
+	private HashMap<String, ControllerButton> buttonmap = new HashMap<>();
+	private void initButtonMap() {
+		buttonmap.put("MOVE-UP", ControllerButton.DPAD_UP);
+		buttonmap.put("MOVE-DOWN", ControllerButton.DPAD_DOWN);
+		buttonmap.put("MOVE-LEFT", ControllerButton.DPAD_LEFT);
+		buttonmap.put("MOVE-RIGHT", ControllerButton.DPAD_RIGHT);
+
+		buttonmap.put("CURSOR-UP", ControllerButton.DPAD_UP);
+		buttonmap.put("CURSOR-DOWN", ControllerButton.DPAD_DOWN);
+		buttonmap.put("CURSOR-LEFT", ControllerButton.DPAD_LEFT);
+		buttonmap.put("CURSOR-RIGHT", ControllerButton.DPAD_RIGHT);
+
+		buttonmap.put("SELECT", ControllerButton.A);
+		buttonmap.put("EXIT", ControllerButton.B);
+
+		buttonmap.put("ATTACK", ControllerButton.A);
+		buttonmap.put("MENU", ControllerButton.X);
+		buttonmap.put("CRAFT", ControllerButton.Y);
+		buttonmap.put("PICKUP", ControllerButton.LEFTBUMPER);
+
+		buttonmap.put("PAUSE", ControllerButton.START);
 	}
 
 	public boolean buttonPressed(ControllerButton button) {
