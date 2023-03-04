c483efe81fb84c3a2fdb11120c55818e70a36d94
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-03-04 23:46:44 +0800

    Create empty Chicken.java

---
+++ src/main/java/minicraft/entity/mob/Chicken.java
@@ -0,0 +1,9 @@
+package minicraft.entity.mob;
+
+import minicraft.gfx.SpriteLinker;
+
+public class Chicken extends PassiveMob {
+	public Chicken(SpriteLinker.LinkedSprite[][] sprites) {
+		super(sprites);
+	}
+}
