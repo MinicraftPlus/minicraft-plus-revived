MinicraftPlus/minicraft-plus-revived

commit 9de1e2a200cec588d80e21d9a2bd4b3df976104d

Graphics: Revert to setting fps on startup

src/main/java/minicraft/core/io/Settings.java
@@ -1,20 +1,18 @@
 package minicraft.core.io;
 
-import minicraft.core.Initializer;
 import minicraft.screen.entry.ArrayEntry;
 import minicraft.screen.entry.BooleanEntry;
+import minicraft.screen.entry.RangeEntry;
 
-import java.awt.DisplayMode;
-import java.awt.GraphicsEnvironment;
-import java.util.ArrayList;
+import java.awt.*;
 import java.util.HashMap;
 
-public class Settings {
+public final class Settings {
 
 	private static final HashMap<String, ArrayEntry<?>> options = new HashMap<>();
 
 	static {
-		options.put("fps", new FPSEntry("minicraft.settings.fps")); // Has to check if the game is running in a headless mode. If it doesn't set the fps to 60
+		options.put("fps", new RangeEntry("minicraft.settings.fps", 10, 300, getDefaultRefreshRate())); // Has to check if the game is running in a headless mode. If it doesn't set the fps to 60
 		options.put("screenshot", new ArrayEntry<>("minicraft.settings.screenshot_scale", 1, 2, 5, 10, 15, 20)); // The magnification of screenshot. I would want to see ultimate sized.
 		options.put("diff", new ArrayEntry<>("minicraft.settings.difficulty", "minicraft.settings.difficulty.easy", "minicraft.settings.difficulty.normal", "minicraft.settings.difficulty.hard"));
 		options.get("diff").setSelection(1);
@@ -36,151 +34,68 @@ public class Settings {
 		options.put("quests", new BooleanEntry("Quests", false));
 		options.put("showquests", new BooleanEntry("Quests Panel", true));
 
+		options.put("language", new ArrayEntry<>("minicraft.settings.language", true, false, Localization.getLocales()));
+		options.get("language").setValue(Localization.getSelectedLanguage());
+
 		options.get("mode").setChangeAction(value ->
 			options.get("scoretime").setVisible("minicraft.settings.mode.score".equals(value))
 		);
 	}
 
-	// Returns the value of the specified option
+	/**
+	 * Returns the value of the specified option.
+	 * @param option The setting to get.
+	 * @return The value of the setting
+	 */
 	public static Object get(String option) { return options.get(option.toLowerCase()).getValue(); }
 
-	// Returns the index of the value in the list of values for the specified option
+	/**
+	 * Returns the index of the value in the list of values for the specified option.
+	 * @param option The setting to get.
+	 * @return The index of the setting.
+	 */
 	public static int getIdx(String option) { return options.get(option.toLowerCase()).getSelection(); }
 
-	// Return the ArrayEntry object associated with the given option name.
+	/**
+	 * Return the ArrayEntry object associated with the given option name.
+	 * @param option The setting to get.
+	 * @return The ArrayEntry.
+	 */
 	public static ArrayEntry<?> getEntry(String option) { return options.get(option.toLowerCase()); }
 
-	// Sets the value of the given option name, to the given value, provided it is a valid value for that option.
+	/**
+	 * Sets the value of the given option name, to the given value, provided it is a valid value for that option.
+	 * @param option The setting to edit.
+	 * @param value The value to change to.
+	 */
 	public static void set(String option, Object value) {
 		options.get(option.toLowerCase()).setValue(value);
 	}
 
-	// Sets the index of the value of the given option, provided it is a valid index
+	/**
+	 * Sets the index of the value of the given option, provided it is a valid index.
+	 * @param option The setting to edit.
+	 * @param idx Index to select.
+	 */
 	public static void setIdx(String option, int idx) {
 		options.get(option.toLowerCase()).setSelection(idx);
 	}
 
-	public static final int FPS_UNLIMITED = -1;
-
 	/**
-	 * Getting the set value for maximum framerate.
-	 * @return The set or device framerate value, or {@link #FPS_UNLIMITED} if unlimited.
+	 * Gets the refresh rate of the default monitor.
+	 * Safely handles headless environments (if that were to happen for some reason).
+	 * @return The refresh rate if successful. 60 if not.
 	 */
-	public static int getFPS() {
-		return ((FPSEntry) getEntry("fps")).getFPSValue();
-	}
-
-	private static class FPSEntry extends ArrayEntry<FPSEntry.FPSValue> {
-		public static class FPSValue {
-			private int fps = DisplayMode.REFRESH_RATE_UNKNOWN;
-			public enum ValueType { Normal, VSync, Unlimited; }
-			private final ValueType type;
-
-			public FPSValue(ValueType type) {
-				this.type = type == ValueType.Normal ? ValueType.Unlimited : type;
-			}
-			public FPSValue(int fps) {
-				type = ValueType.Normal;
-				if (fps > 300) fps = 300;
-				if (fps < 10) fps = 10;
-				this.fps = fps;
-			}
-
-			public int getFPS() {
-				if (type == ValueType.VSync) {
-					// There is currently no actual way to do VSync by only Swing/AWT.
-					// Instead, the device refresh rate is used.
-					int rate = Initializer.getFrame().getGraphicsConfiguration().getDevice().getDisplayMode().getRefreshRate();
-					return rate == DisplayMode.REFRESH_RATE_UNKNOWN ? FPS_UNLIMITED : rate;
-				} else if (type == ValueType.Unlimited) {
-					return FPS_UNLIMITED;
-				} else {
-					return fps;
-				}
-			}
-
-			@Override
-			public String toString() {
-				return type == ValueType.Normal ? String.valueOf(fps) : type.toString();
-			}
-
-			@Override
-			public boolean equals(Object obj) {
-				if (obj == null) return false;
-				if (this == obj) return true;
-				if (obj instanceof FPSValue)
-					return type == ((FPSValue) obj).type &&
-						fps == ((FPSValue) obj).fps;
-				return false;
-			}
-
-			@Override
-			public int hashCode() {
-				return type.ordinal() * fps + fps;
-			}
+	private static int getDefaultRefreshRate() {
+		int hz;
+		try {
+			hz = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate();
+		} catch (HeadlessException e) {
+			return 60;
 		}
 
-		private static FPSValue[] getArray() {
-			ArrayList<FPSValue> list = new ArrayList<>();
-			for (int i = 10; i <= 300; i += 10) {
-				list.add(new FPSValue(i));
-			}
-
-			list.add(new FPSValue(FPSValue.ValueType.VSync));
-			list.add(new FPSValue(FPSValue.ValueType.Unlimited));
-
-			return list.toArray(new FPSValue[0]);
-		}
-
-		public FPSEntry(String label) {
-			super(label, false, getArray());
-			int rate = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate();
-			setValue(rate == DisplayMode.REFRESH_RATE_UNKNOWN ? 60 : rate % 10 == 0 ? rate : rate - rate % 10);
-		}
-
-		/**
-		 * Getting the set value for maximum framerate.
-		 * @return The set or device framerate value, or {@link #FPS_UNLIMITED} if unlimited.
-		 */
-		public int getFPSValue() {
-			return getValue().getFPS();
-		}
-
-		@Override
-		public void setValue(Object value) {
-			if (value instanceof FPSValue) {
-				super.setValue(value);
-			} else {
-				if (!(value instanceof FPSValue.ValueType)) try { // First tests if it is an integral value.
-					int v;
-					if (value instanceof Integer) v = (int) value;
-					else v = Integer.parseInt(value.toString());
-					if (v < 10) v = 10;
-					if (v > 300) v = 300;
-					v -= v % 10;
-					for (FPSValue val : options) {
-						if (val.fps == v) {
-							super.setValue(val);
-							return;
-						}
-					}
-				} catch (NumberFormatException ignored) {}
-
-				try { // Then falls back to enums.
-					FPSValue.ValueType type;
-					if (value instanceof FPSValue.ValueType) type = (FPSValue.ValueType) value;
-					else type = FPSValue.ValueType.valueOf(value.toString());
-					if (type != FPSValue.ValueType.Normal) for (FPSValue val : options) {
-						if (val.type == type) {
-							super.setValue(val);
-							return;
-						}
-					}
-				} catch (IllegalArgumentException ignored) {}
-
-				// Finally VSync is applied.
-				super.setValue(FPSValue.ValueType.VSync);
-			}
-		}
+		if (hz == DisplayMode.REFRESH_RATE_UNKNOWN) return 60;
+		if (hz > 300 || 10 >= hz) return 60;
+		return hz;
 	}
 }

src/main/java/minicraft/saveload/Load.java
@@ -517,7 +517,7 @@ public class Load {
 		// Settings
 		Settings.set("sound", json.getBoolean("sound"));
 		Settings.set("autosave", json.getBoolean("autosave"));
-		Settings.set("fps", json.getString("fps"));
+		Settings.set("fps", json.getInt("fps"));
 		Settings.set("showquests", json.optBoolean("showquests", true));
 
 		if (json.has("lang")) {

src/main/java/minicraft/core/Game.java
@@ -86,7 +86,7 @@ public class Game {
 		World.resetGame(); // "half"-starts a new game, to set up initial variables
 		player.eid = 0;
 		new Load(true); // This loads any saved preferences.
-		MAX_FPS = Settings.getFPS(); // DO NOT put this above.
+		MAX_FPS = (int) Settings.get("fps"); // DO NOT put this above.
 
 		// Update fullscreen frame if Updater.FULLSCREEN was updated previously
 		if (Updater.FULLSCREEN) {

src/main/java/minicraft/core/Initializer.java
@@ -96,7 +96,7 @@ public class Initializer extends Game {
 			}
 
 			now = System.nanoTime();
-			if (MAX_FPS == Settings.FPS_UNLIMITED || now >= lastRender + 1E9D / MAX_FPS) {
+			if (now >= lastRender + 1E9D / MAX_FPS) {
 				frames++;
 				lastRender = now;
 				Renderer.render();

src/main/java/minicraft/screen/OptionsMainMenuDisplay.java
@@ -27,6 +27,6 @@ public class OptionsMainMenuDisplay extends Display {
     @Override
     public void onExit() {
         new Save();
-        Game.MAX_FPS = Settings.getFPS();
+        Game.MAX_FPS = (int) Settings.get("fps");
     }
 }

src/main/java/minicraft/screen/OptionsWorldDisplay.java
@@ -74,6 +74,6 @@ public class OptionsWorldDisplay extends Display {
 	@Override
 	public void onExit() {
 		new Save();
-		Game.MAX_FPS = Settings.getFPS();
+		Game.MAX_FPS = (int) Settings.get("fps");
 	}
 }

src/main/java/minicraft/screen/entry/ArrayEntry.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class ArrayEntry<T> extends ListEntry {
 
 	private final String label;
-	protected T[] options;
+	private T[] options;
 	private boolean[] optionVis;
 
 	private int selection;

commit 97ccc5c0dcc817095bd6ba17460f16dfed946ac2

Fix problems caused by 9de1e2a
See https://github.com/MinicraftPlus/minicraft-plus-revived/commit/9de1e2a200cec588d80e21d9a2bd4b3df976104d

build.gradle
@@ -6,7 +6,7 @@ plugins {
 }
 
 
-project.version = '2.2.0-dev3'
+project.version = '2.2.0-dev4'
 group = "java.minicraft"
 archivesBaseName = "minicraft-plus"
 

src/main/java/minicraft/core/io/Settings.java
@@ -12,7 +12,7 @@ public final class Settings {
 	private static final HashMap<String, ArrayEntry<?>> options = new HashMap<>();
 
 	static {
-		options.put("fps", new RangeEntry("minicraft.settings.fps", 10, 300, getDefaultRefreshRate())); // Has to check if the game is running in a headless mode. If it doesn't set the fps to 60
+		options.put("fps", new RangeEntry("minicraft.settings.fps", 10, 300, getDefaultRefreshRate(), 10)); // Has to check if the game is running in a headless mode. If it doesn't set the fps to 60
 		options.put("screenshot", new ArrayEntry<>("minicraft.settings.screenshot_scale", 1, 2, 5, 10, 15, 20)); // The magnification of screenshot. I would want to see ultimate sized.
 		options.put("diff", new ArrayEntry<>("minicraft.settings.difficulty", "minicraft.settings.difficulty.easy", "minicraft.settings.difficulty.normal", "minicraft.settings.difficulty.hard"));
 		options.get("diff").setSelection(1);
@@ -79,20 +79,25 @@ public final class Settings {
 	}
 
 	/**
-	 * Gets the refresh rate of the default monitor.
+	 * Gets the double of the refresh rate of the default monitor and rounds down the value to the nearest 10.
 	 * Safely handles headless environments (if that were to happen for some reason).
-	 * @return The refresh rate if successful. 60 if not.
+	 * @return The refresh rate if successful. 120 if not. 120 (double) is more optimal than 60.
 	 */
-	private static int getDefaultRefreshRate() {
+	public static int getDefaultRefreshRate() {
 		int hz;
 		try {
-			hz = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate();
+			hz = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate()
+				* 2; // Double the refresh rate as the max FPS value, which could make the rendering optimal without unstable tearing.
 		} catch (HeadlessException e) {
-			return 60;
+			return 120;
 		}
 
-		if (hz == DisplayMode.REFRESH_RATE_UNKNOWN) return 60;
-		if (hz > 300 || 10 >= hz) return 60;
-		return hz;
+		if (hz == DisplayMode.REFRESH_RATE_UNKNOWN) return 120;
+		if (hz > 300) {
+			return 300;
+		} else if (10 >= hz) {
+			return 10;
+		}
+		return hz % 10 == 0 ? hz : hz - hz % 10;
 	}
 }

src/main/java/minicraft/saveload/Load.java
@@ -60,6 +60,7 @@ import minicraft.screen.ResourcePackDisplay;
 import minicraft.screen.SkinDisplay;
 import minicraft.screen.TutorialDisplayHandler;
 import minicraft.screen.entry.ListEntry;
+import minicraft.screen.entry.RangeEntry;
 import minicraft.screen.entry.StringEntry;
 import minicraft.util.AdvancementElement;
 import minicraft.util.Logging;
@@ -517,7 +518,18 @@ public class Load {
 		// Settings
 		Settings.set("sound", json.getBoolean("sound"));
 		Settings.set("autosave", json.getBoolean("autosave"));
-		Settings.set("fps", json.getInt("fps"));
+		if (prefVer.compareTo(new Version("2.2.0-dev3")) == 0) { // Setting exists only in 2.2.0-dev3
+			String fps = json.getString("fps");
+			if (fps.equals("Unlimited")) {
+				Settings.set("fps", ((RangeEntry) Settings.getEntry("fps")).max);
+			} else if (fps.equals("VSync")) {
+				Settings.set("fps", Settings.getDefaultRefreshRate());
+			} else {
+				Settings.set("fps", Integer.parseInt(fps));
+			}
+		} else {
+			Settings.set("fps", json.getInt("fps"));
+		}
 		Settings.set("showquests", json.optBoolean("showquests", true));
 
 		if (json.has("lang")) {

src/main/java/minicraft/screen/entry/RangeEntry.java
@@ -1,31 +1,34 @@
 package minicraft.screen.entry;
 
 public class RangeEntry extends ArrayEntry<Integer> {
-	
-	private static Integer[] getIntegerArray(int min, int max) {
-		Integer[] ints = new Integer[max - min + 1];
-		
+
+	private static Integer[] getIntegerArray(int min, int max, int interval) {
+		Integer[] ints = new Integer[(max - min) / interval + 1];
+
 		for (int i = 0; i < ints.length; i++)
-			ints[i] = min+i;
-		
+			ints[i] = min + i*interval;
+
 		return ints;
 	}
-	
-	private int min, max;
-	
-	public RangeEntry(String label, int min, int max, int initial) {
-		super(label, false, getIntegerArray(min, max));
-		
+
+	public final int min, max;
+	private final int interval;
+
+	public RangeEntry(String label, int min, int max, int initial) { this(label, min, max, initial, 1); }
+	public RangeEntry(String label, int min, int max, int initial, int interval) {
+		super(label, false, getIntegerArray(min, max, interval));
+
 		this.min = min;
 		this.max = max;
-		
+		this.interval = interval;
+
 		setValue(initial);
 	}
-	
+
 	@Override
 	public void setValue(Object o) {
 		if (!(o instanceof Integer)) return;
-		
-		setSelection(((Integer)o)-min);
+
+		setSelection((((Integer)o)-min) / interval);
 	}
 }