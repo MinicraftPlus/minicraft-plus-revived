3b8f9f1efeee6562f23fe7aad5a12fed82a8b16a
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-07-12 11:20:55 +0800

    Fix Obsidian Knight phase problem
    Also fix sprite setting problem

--- src/client/java/minicraft/entity/mob/ObsidianKnight.java
+++ src/client/java/minicraft/entity/mob/ObsidianKnight.java
@@ -16,6 +16,7 @@ import minicraft.gfx.Screen;
 import minicraft.gfx.SpriteLinker;
 import minicraft.item.Items;
 import minicraft.screen.AchievementsDisplay;
+import org.jetbrains.annotations.Range;
 
 public class ObsidianKnight extends EnemyMob {
 	private static final SpriteLinker.LinkedSprite[][][] armored = new SpriteLinker.LinkedSprite[][][] {
@@ -36,8 +37,9 @@ public class ObsidianKnight extends EnemyMob {
 	public static boolean beaten = false; // If the boss was beaten
 	public static boolean active = false; // If the boss is active
 
-	private static int phase = 0; // The phase of the boss. {0, 1}
-	private static int attackPhaseCooldown = 0; // Cooldown between attacks
+	@Range(from = 0, to = 1)
+	private int phase = 0; // The phase of the boss. {0, 1}
+	private int attackPhaseCooldown = 0; // Cooldown between attacks
 
 	private AttackPhase attackPhase = AttackPhase.Attacking;
 	private enum AttackPhase { Attacking, Dashing, Walking; } // Using fire sparks in attacking.
@@ -81,12 +83,9 @@ public class ObsidianKnight extends EnemyMob {
 		if (health <= 2500) {
 			phase = 1;
 		}
-		if (phase == 1) {
+		if (phase == 1) { // Refreshing phased sprites
 			lvlSprites = broken;
-		}
-
-		//Fix for wrong sprite usage when respawned
-		if (health > 2500) {
+		} else {
 			lvlSprites = armored;
 		}
 
@@ -139,7 +138,7 @@ public class ObsidianKnight extends EnemyMob {
 				else if (atan2 < -67.5) attackDir = -90;
 				else if (atan2 < -22.5) attackDir = -45;
 				else attackDir = 0;
-				double speed = 1 + attackLevel * 0.2 + attackTime / 10 * 0.01; // speed is dependent on the attackType. (higher attackType, faster speeds)
+				double speed = 1 + attackLevel * 0.2 + attackTime / 10D * 0.01; // speed is dependent on the attackType. (higher attackType, faster speeds)
 				// The range of attack is 90 degrees. With little random factor.
 				int phi = attackDir - 36 + (attackTime % 5) * 18 + random.nextInt(7) - 3;
 				level.add(new FireSpark(this, Math.cos(Math.toRadians(phi)) * speed, Math.sin(Math.toRadians(phi)) * speed)); // Adds a spark entity with the cosine and sine of dir times speed.
