e6be928ebd62968c66593f41e6e0f5e3badc73c5
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-07-18 17:21:41 +0800

    Fix crop stage calculation error

--- src/client/java/minicraft/level/tile/farming/TomatoTile.java
+++ src/client/java/minicraft/level/tile/farming/TomatoTile.java
@@ -22,7 +22,7 @@ public class TomatoTile extends CropTile {
 	public void render(Screen screen, Level level, int x, int y) {
 		int age = (level.getData(x, y) >> 3) & maxAge;
 		Tiles.get("Farmland").render(screen, level, x, y);
-		int stage = age / (maxAge / 3);
+		int stage = (int) (age / maxAge * 3f);
 		screen.render(x * 16, y * 16, spritStages[stage]);
 	}
 }
