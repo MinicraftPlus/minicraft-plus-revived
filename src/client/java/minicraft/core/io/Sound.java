7b86af755491db8b1509568bb132c48c0d69f937
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-07-27 03:48:22 +0800

    Add multiple clip support

--- src/client/java/minicraft/core/io/Sound.java
+++ src/client/java/minicraft/core/io/Sound.java
@@ -4,7 +4,9 @@ import minicraft.core.CrashHandler;
 import minicraft.util.Logging;
 import org.jetbrains.annotations.Nullable;
 
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
@@ -13,6 +15,9 @@ import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import java.applet.AudioClip;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
@@ -21,10 +26,16 @@ public class Sound {
 	// Creates sounds from their respective files
 	private static final HashMap<String, Sound> sounds = new HashMap<>();
 
-	private Clip clip; // Creates a audio clip to be played
+	private final DataLine.Info info;
+	private final byte[] raw;
+	private final AudioFormat format;
+	private final long length;
 
-	private Sound(Clip clip) {
-		this.clip = clip;
+	private Sound(DataLine.Info info, byte[] raw, AudioFormat format, long length) {
+		this.info = info;
+		this.raw = raw;
+		this.format = format;
+		this.length = length;
 	}
 
 	public static void resetSounds() {
@@ -33,7 +44,9 @@ public class Sound {
 
 	public static void loadSound(String key, InputStream in, String pack) {
 		try {
-			DataLine.Info info = new DataLine.Info(Clip.class, AudioSystem.getAudioFileFormat(in).getFormat());
+			AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(in);
+			AudioFormat format = fileFormat.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
 
 			if (!AudioSystem.isLineSupported(info)) {
 				Logging.RESOURCEHANDLER_SOUND.error("ERROR: Audio format of file \"{}\" in pack \"\" is not supported: {}", key, pack,  AudioSystem.getAudioFileFormat(in));
@@ -65,25 +78,21 @@ public class Sound {
 				return;
 			}
 
-			Clip clip = (Clip)AudioSystem.getLine(info);
-			clip.open(AudioSystem.getAudioInputStream(in));
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] buf = new byte[8192];
+			int length;
+			while ((length = in.read(buf)) != -1) {
+				out.write(buf, 0, length);
+			}
+			sounds.put(key, new Sound(info, out.toByteArray(), format, fileFormat.getFrameLength()));
 
-			clip.addLineListener(e -> {
-				if (e.getType() == LineEvent.Type.STOP) {
-					clip.flush();
-					clip.setFramePosition(0);
-				}
-			});
-
-			sounds.put(key, new Sound(clip));
-
-		} catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+		} catch (UnsupportedAudioFileException | IOException e) {
 			CrashHandler.errorHandle(e, new CrashHandler.ErrorInfo("Audio Could not Load", CrashHandler.ErrorInfo.ErrorType.REPORT,
 				String.format("Could not load audio: %s in pack: %s", key, pack)));
 		}
 	}
 
-	/** Recommend {@link #play(String)} and {@link #loop(String, boolean)}. */
+	/** Recommend {@link #play(String)} and {@link #loop(String, int)}. */
 	@Nullable
 	public static Sound getSound(String key) {
 		return sounds.get(key);
@@ -95,27 +104,46 @@ public class Sound {
 		if (sound != null) sound.play();
 	}
 
-	/** This method does safe check for {@link #loop(boolean)}. */
-	public static void loop(String key, boolean start) {
+	/** This method does safe check for {@link #loop(int)}. */
+	public static void loop(String key, int count) {
 		Sound sound = sounds.get(key);
-		if (sound != null) sound.loop(start);
+		if (sound != null) sound.loop(count);
+	}
+
+	@Nullable
+	private Clip createClip() {
+		try {
+			Clip clip = (Clip) AudioSystem.getLine(info); // Creates an audio clip to be played
+			clip.open(new AudioInputStream(new ByteArrayInputStream(raw), format, length));
+			clip.addLineListener(e -> {
+				if (e.getType() == LineEvent.Type.STOP) {
+					clip.flush();
+					clip.close();
+				}
+			});
+
+			return clip;
+		} catch (LineUnavailableException | IOException e) {
+			Logging.RESOURCEHANDLER_SOUND.error(e, "Unable to create Clip");
+			return null;
+		}
 	}
 
 	public void play() {
-		if (!(boolean)Settings.get("sound") || clip == null) return;
+		if (!(boolean)Settings.get("sound")) return;
 
-		if (clip.isRunning() || clip.isActive())
-			clip.stop();
-
-		clip.start();
+		Clip clip = createClip();
+		if (clip != null) {
+			clip.start();
+		}
 	}
 
-	public void loop(boolean start) {
-		if (!(boolean)Settings.get("sound") || clip == null) return;
+	public void loop(int count) {
+		if (!(boolean)Settings.get("sound")) return;
 
-		if (start)
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		else
-			clip.stop();
+		Clip clip = createClip();
+		if (clip != null) {
+			clip.loop(count);
+		}
 	}
 }
