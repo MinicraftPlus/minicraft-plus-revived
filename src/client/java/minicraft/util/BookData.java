0a106d66d09b2a74eff06995aa398519b88a7e50
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-08-15 23:56:46 +0800

    Add LoreBook class

--- src/client/java/minicraft/util/BookData.java
+++ src/client/java/minicraft/util/BookData.java
@@ -2,6 +2,8 @@ package minicraft.util;
 
 import minicraft.item.BookItem;
 
+import java.util.HashMap;
+
 public class BookData {
 
 	public static BookItem.BookContent about;
@@ -10,12 +12,44 @@ public class BookData {
 	public static BookItem.BookContent antVenomBook;
 	public static BookItem.BookContent storylineGuide;
 
+	private static final HashMap<String, LoreBook> loreBooks = new HashMap<>();
+
+	public static final LoreBookKey lore1 = new LoreBookKey("lore1");
+	public static final LoreBookKey lore2 = new LoreBookKey("lore2");
+
+	public static class LoreBook {
+		public final String title; // First line is the title.
+		public final BookItem.BookContent content; // The text left is the content.
+
+		public LoreBook(String title, String content) {
+			this.title = title;
+			this.content = () -> content;
+		}
+	}
+
+	public static class LoreBookKey {
+		private final String key;
+
+		private LoreBookKey(String key) {
+			this.key = key;
+		}
+
+		public final LoreBook getBook() {
+			return loreBooks.get(key);
+		}
+
+		public final void loadBook(LoreBook book) {
+			loreBooks.put(key, book);
+		}
+	}
+
 	public static void resetBooks() {
 		about = null;
 		credits = null;
 		instructions = null;
 		antVenomBook = null;
 		storylineGuide = null;
+		loreBooks.clear();
 	}
 
 	public static String loadBook(String content) {
