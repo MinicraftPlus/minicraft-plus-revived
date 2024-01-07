f7bde51423de1cf5bd1c740601246ec18d51fa97
GreenEye <thebigeyedev@gmail.com>
2024-01-07 03:31:19 -0300

    Corrected import

--- src/client/java/minicraft/entity/furniture/Tnt.java
+++ src/client/java/minicraft/entity/furniture/Tnt.java
@@ -11,6 +11,7 @@ import minicraft.gfx.Screen;
 import minicraft.gfx.SpriteLinker.LinkedSprite;
 import minicraft.gfx.SpriteLinker.SpriteType;
 import minicraft.item.Item;
+import minicraft.item.PowerGloveItem;
 import minicraft.level.Level;
 import minicraft.level.tile.Tile;
 import minicraft.level.tile.Tiles;
