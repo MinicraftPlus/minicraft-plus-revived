a892c39a4508a9e99f7ec86b6dab80b7c007ac92
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-02-10 07:35:09 +0800

    Turn WaterTile check stored to instanceof

--- src/client/java/minicraft/level/tile/WaterTile.java
+++ src/client/java/minicraft/level/tile/WaterTile.java
@@ -27,31 +27,25 @@ public class WaterTile extends Tile {
 		return e.canSwim();
 	}
 
-	private Tile hole = null;
-	private Tile lava = null;
-
 	@Override
 	public boolean tick(Level level, int xt, int yt) {
-		if (hole == null) hole = Tiles.get("Hole");
-		if (lava == null) lava = Tiles.get("Lava");
-
 		int xn = xt;
 		int yn = yt;
 
 		if (random.nextBoolean()) xn += random.nextInt(2) * 2 - 1;
 		else yn += random.nextInt(2) * 2 - 1;
 
-		if (level.getTile(xn, yn) == hole) {
+		if (level.getTile(xn, yn) instanceof HoleTile) {
 			level.setTile(xn, yn, this);
 		}
 
 		// These set only the non-diagonally adjacent lava tiles to obsidian
 		for (int x = -1; x < 2; x++) {
-			if (level.getTile(xt + x, yt) == lava)
+			if (level.getTile(xt + x, yt) instanceof LavaTile)
 				level.setTile(xt + x, yt, Tiles.get("Raw Obsidian"));
 		}
 		for (int y = -1; y < 2; y++) {
-			if (level.getTile(xt, yt + y) == lava)
+			if (level.getTile(xt, yt + y) instanceof LavaTile)
 				level.setTile(xt, yt + y, Tiles.get("Raw Obsidian"));
 		}
 		return false;
