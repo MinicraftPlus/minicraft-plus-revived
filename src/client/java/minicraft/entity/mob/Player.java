2f9de88e16a61f33025b99c8a80c5778048314e8
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-02-17 00:28:37 +0800

    Player hurt controller vibration implementation

--- src/client/java/minicraft/entity/mob/Player.java
+++ src/client/java/minicraft/entity/mob/Player.java
@@ -55,6 +55,7 @@ import minicraft.screen.SkinDisplay;
 import minicraft.screen.WorldSelectDisplay;
 import minicraft.util.AdvancementElement;
 import minicraft.util.Logging;
+import minicraft.util.MyUtils;
 import minicraft.util.Vector2;
 import org.jetbrains.annotations.Nullable;
 
@@ -1172,10 +1173,10 @@ public class Player extends Mob implements ItemHolder, ClientTickable {
 		if (Game.isMode("minicraft.settings.mode.creative") || hurtTime > 0 || Bed.inBed(this))
 			return; // Can't get hurt in creative, hurt cooldown, or while someone is in bed
 
-		int healthDam = 0, armorDam = 0;
+		int healthDam = 0, armorDam = 0, suffered = 0;
 		if (this == Game.player) {
 			if (curArmor == null) { // No armor
-				healthDam = damage; // Subtract that amount
+				suffered = healthDam = damage; // Subtract that amount
 			} else { // Has armor
 				armorDamageBuffer += damage;
 				armorDam += damage;
@@ -1206,6 +1207,8 @@ public class Player extends Mob implements ItemHolder, ClientTickable {
 
 		Sound.play("playerhurt");
 		hurtTime = playerHurtTime;
+		suffered = MyUtils.clamp(suffered, 1, 5);
+		Game.input.controllerVibration(suffered * .1F, suffered * .1F, playerHurtTime * 1000 / 60);
 	}
 
 	/**
