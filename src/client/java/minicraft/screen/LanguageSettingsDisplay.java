7359ecba4f4c5b11fd92aab1e5c01959cee0ba65
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-06-27 19:29:06 +0800

    Update copyright headers

--- src/client/java/minicraft/screen/LanguageSettingsDisplay.java
+++ src/client/java/minicraft/screen/LanguageSettingsDisplay.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2011-2024 Minicraft+ Developers and Contributors
+ * SPDX-FileCopyrightText: 2011-2024 Minicraft+ contributors
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
