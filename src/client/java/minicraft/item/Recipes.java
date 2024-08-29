4e75a1a13900a73073c6bc310bd1864cd1f605cc
Litorom <litorom1@gmail.com>
2024-08-29 15:36:40 -0400

    Added missing recipe for Ornate Wood

--- src/client/java/minicraft/item/Recipes.java
+++ src/client/java/minicraft/item/Recipes.java
@@ -60,6 +60,7 @@ public class Recipes {
 		workbenchRecipes.add(new Recipe("Workbench_1", "Wood_10"));
 		workbenchRecipes.add(new Recipe("Torch_2", "Wood_1", "coal_1"));
 		workbenchRecipes.add(new Recipe("plank_2", "Wood_1"));
+		workbenchRecipes.add(new Recipe("Ornate Wood_1", "Wood_1"));
 		workbenchRecipes.add(new Recipe("Plank Wall_1", "plank_3"));
 		workbenchRecipes.add(new Recipe("Wood Door_1", "plank_5"));
 		workbenchRecipes.add(new Recipe("Wood Fence_1", "plank_3"));
